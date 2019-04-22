8eff30e5fe84ca772ad02ee83a423bac2fd6ba31
Juha Remes <jremes@outlook.com>
2019-04-22 19:58:56 +0100

    Initial commit

---
+++ database/migrations/2019_04_18_225855_create_challenges_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateChallengesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('challenges', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('category');
+            $table->integer('score');
+            $table->string('title');
+            $table->string('flag');
+            $table->string('content');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('challenges');
+    }
+}
