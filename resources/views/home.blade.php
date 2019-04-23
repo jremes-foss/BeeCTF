090bfed27e7a1ce34117d4eba41cbe71b3d6a14e
Juha Remes <jremes@outlook.com>
2019-04-24 00:08:44 +0100

    Few front end fixes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
+<div class="jumbotron text-center">
+    <h1>BeeCTF</h1>
+</div>
 <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
