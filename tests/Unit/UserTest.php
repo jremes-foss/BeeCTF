01c4fd920492ea233a85dc8a19eaf6186f72495e
Juha Remes <jremes@outlook.com>
2019-05-01 23:12:09 +0100

    Add UserTest.php

---
+++ tests/Unit/UserTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class UserTest extends TestCase
+{
+    public function testDefaultUserIsNotAdmin()
+    {
+    	// TODO
+    }
+
+    public function testAdminUserIsAnAdmin()
+    {
+    	// TODO
+    }
+}
