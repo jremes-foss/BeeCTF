060b3fb2b40a0ad4d82e6061ce2f6a6f95220272
Juha Remes <jremes@outlook.com>
2019-05-01 22:53:53 +0100

    Write store method for ChallengeController.php

--- app/Http/Controllers/ChallengeController.php
+++ app/Http/Controllers/ChallengeController.php
@@ -6,5 +6,23 @@ use Illuminate\Http\Request;
 
 class ChallengeController extends Controller
 {
-    //
+    public function store(Request $request)
+    {
+    	$request->validate([
+    		'topic' => 'required',
+    		'category' => 'required',
+    		'points' => 'required',
+    		'description' => 'required'
+    	]);
+
+    	$challenge = new Challenge([
+    		'topic' => $request->get('topic'),
+    		'category' => $request->get('category'),
+    		'points' => $request->get('points'),
+    		'description' => $request->get('description')
+    	]);
+
+    	$challenge->save();
+    	return redirect('/challenges')->with('success', 'Challenge saved!');
+    }
 }
