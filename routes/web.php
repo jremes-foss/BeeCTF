f97a758386955d48c65149676ce0ce6fe45533fb
Juha Remes <jremes@outlook.com>
2019-05-02 22:36:23 +0100

    Call the Middleware in web.php route

--- routes/web.php
+++ routes/web.php
@@ -18,4 +18,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
-Route::get('/admin', 'HomeController@admin')->name('admin');
+
+Route::get('/admin', 'HomeController@admin')
+	->middleware('is_admin')
+	->name('admin');
