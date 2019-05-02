ce60f9159287f896a9564b032937f6844e1b65b5
Juha Remes <jremes@outlook.com>
2019-05-02 22:21:56 +0100

    Add unit tests for admin view

---
+++ tests/Unit/AdminTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class AdminTest extends TestCase
+{
+	use RefreshDatabase;
+
+	public function testUserCannotAccessAdmin()
+	{
+		$user = factory(User::class)->create();
+		$this->actingAs($user)
+			->get('/admin')
+			->assertRedirect('home');
+	}
+
+	public function testAdminCanAccessAdmin()
+	{
+		$admin = factory(User::class)
+			->states('admin');
+			->create();
+
+		$this->actingAs('admin')
+			->get('/admin');
+			->assertStatus(200);
+	}
+}
