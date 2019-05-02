ee0edf061dc992b5331822105997eadd7b6d6d87
Juha Remes <jremes@outlook.com>
2019-05-02 22:34:00 +0100

    Redirect home if user is not admin

--- app/Http/Middleware/IsAdmin.php
+++ app/Http/Middleware/IsAdmin.php
@@ -15,6 +15,10 @@ class IsAdmin
      */
     public function handle($request, Closure $next)
     {
-        return $next($request);
+        if(auth()->user()->isAdmin()) {
+            return $next($request);
+        }
+
+        return redirect('home');
     }
 }
