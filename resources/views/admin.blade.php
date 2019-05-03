94e7f9b318ae15bb385a3a340fb00ec7d0763db4
Juha Remes <jremes@outlook.com>
2019-05-03 20:50:26 +0100

    Tweaks

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -7,7 +7,7 @@
     <div class="flex items-center justify-center h-9/10">
         <div class="text-center">
             <h1 class="text-grey-darkest text-4xl mb-6">Admin</h1>
-            <p class="uppercase tracking-wide text-sm text-grey-darkest ">For admin users only!</p>
+            <p class="uppercase tracking-wide text-sm text-grey-darkest ">For Admins only!</p>
         </div>
     </div>
 @endsection
