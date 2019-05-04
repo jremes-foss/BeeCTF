e861a6e7cfee4967624489f97cc21f1a3942664c
Juha Remes <jremes@outlook.com>
2019-05-04 02:23:01 +0100

    Add sidebar - needs fixing, buggy and ugly AF

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -1,5 +1,9 @@
 @extends('layouts.app')
 
+<div id="sidebar">
+@include('layouts.sidebar')
+</div>
+
 @section('content')
 	<div class="container text-center">
 	    <h1>Admin Section</h1>
---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,23 @@
+<div class="sidebar">
+    <nav class="sidebar-nav">
+        <ul class="nav">
+            <li class="nav-item">
+                <a class="nav-link active" href="/dashboard">
+                    <i class="nav-icon icon-speedometer"></i> Dashboard
+                </a>
+            </li>
+            <li class="nav-title">Settings</li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">
+                    <i class="nav-icon icon-user"></i> Users
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">
+                    <i class="nav-icon icon-lock"></i> Challenges
+                </a>
+            </li>
+        </ul>
+    </nav>
+    <button class="sidebar-minimizer brand-minimizer" type="button"></button>
+</div>
