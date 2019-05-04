b6aaaa6852c50b3ae8fb5fd4f162ddc13452bd07
Juha Remes <jremes@outlook.com>
2019-05-04 01:50:44 +0100

    Remove placeholder content

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -4,10 +4,4 @@
 	<div class="container text-center">
 	    <h1>Admin Section</h1>
 	</div>
-    <div class="flex items-center justify-center h-9/10">
-        <div class="text-center">
-            <h1 class="text-grey-darkest text-4xl mb-6">Admin</h1>
-            <p class="uppercase tracking-wide text-sm text-grey-darkest ">For Admins only!</p>
-        </div>
-    </div>
 @endsection
