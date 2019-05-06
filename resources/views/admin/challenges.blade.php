86007f654a29d53a4151b8732d37598f3894fd37
Juha Remes <jremes@outlook.com>
2019-05-06 19:38:11 +0100

    Change hard-coded dummy content with dynamic data

--- resources/views/admin/challenges.blade.php
+++ resources/views/admin/challenges.blade.php
@@ -19,30 +19,17 @@
 		    </tr>
 		  </thead>
 		  <tbody>
-		    <tr>
-		      <th scope="row">1</th>
-		      <td>Forensics</td>
-		      <td>350</td>
-		      <td>Wireshark</td>
-		      <td>FLAG{w1r3sh4rk_1s_c00l!}</td>
-		      <td>Can you find a flag with Wireshark?</td>
-		    </tr>
-		    <tr>
-		      <th scope="row">2</th>
-		      <td>Web</td>
-		      <td>100</td>
-		      <td>SQL Injection</td>
-		      <td>FLAG{s4n1t1z3_y0ur_1nput!}</td>
-		      <td>Can you find a flag from the database?</td>
-		    </tr>
-		    <tr>
-		      <th scope="row">3</th>
-		      <td>Crypto</td>
-		      <td>300</td>
-		      <td>RSA</td>
-		      <td>FLAG{m1nd_th3_k3ys!}</td>
-		      <td>Can you break this ciphertext?</td>
-		    </tr>
+		  	@foreach($challenges as $challenge)
+		  	<tr>
+		  		<th scope="row">{{ $challenge->id }}</th>
+		  		<td>{{ $challenge->category }}</td>
+		  		<td>{{ $challenge->score }}</td>
+		  		<td>{{ $challenge->title }}</td>
+		  		<td>{{ $challenge->flag }}</td>
+		  		<td>{{ $challenge->content }}</td>
+		  	</tr>
+
+		  	@endforeach
 		  </tbody>
 		</table>
 	</div>
