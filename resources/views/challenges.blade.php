ed9f6a5564a4258bfb7d9fed9e6b7591615289c7
Juha Remes <jremes@outlook.com>
2019-05-09 21:33:38 +0100

    Add modal structure for flag submission

--- resources/views/challenges.blade.php
+++ resources/views/challenges.blade.php
@@ -22,4 +22,25 @@
 		@endforeach
 	</div>
 </div>
+
+<!-- Submit Flag Modal -->
+<div class="modal fade" id="flagValidation" tabindex="-1" role="dialog" aria-labelledby="flagValidationLabel">
+	<div class="modal-dialog" role="document">
+		<div class="modal-content">
+			<div class="modal-header">
+				<h5 class="modal-title" id="exampleModalLabel">Submit Flag</h5>
+				<button type="button" class="close" data-dismiss="modal" aria-label="Cancel">
+					<span aria-hidden="true">&times;</span>
+				</button>
+			</div>
+			<div class="modal-body">
+				Please make sure your flag does not have any typos before submission.
+			</div>
+			<div class="modal-footer">
+				<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+				<button type="button" class="btn btn-primary">Save changes</button>
+			</div>
+		</div>
+	</div>
+</div>
 @endsection
