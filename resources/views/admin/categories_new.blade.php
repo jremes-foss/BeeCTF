34683284392b22b0abfdd2972df46b6ef3fffcc8
Juha Remes <jremes@outlook.com>
2019-05-24 23:05:43 +0100

    Add categories_new.blade.php

---
+++ resources/views/admin/categories_new.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.app')
+@section('content')
+<div id="sidebar" class="col-md-4">
+	@include('layouts.sidebar')
+</div>
+<div class="container">
+	<div class="row">
+		<form role="form" method="post" action="{{ route('admin.new_challenge.store') }}">
+			{{ csrf_field() }}
+			<div class="form-group">
+				<label for="inputCategory">
+					Category
+				</label>
+				<div class="input-group Category">
+					<span class="input-group-addon"><i class="fa fa-address-book"></i></span><input type="text" class="form-control" id="inputCategory" name="inputCategory" placeholder="Enter Category">
+				</div>
+				<input type="submit" class="submit" value="Submit">
+			</div>
+		</form>
+	</div>
+</div>
+@endsection
