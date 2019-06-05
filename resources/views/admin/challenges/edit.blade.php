0c3402adb007fa5030ecb0d834e6d03f5656d0f6
Juha Remes <jremes@outlook.com>
2019-06-05 22:05:42 +0100

    Switch form method: post -> patch

--- resources/views/admin/challenges/edit.blade.php
+++ resources/views/admin/challenges/edit.blade.php
@@ -6,7 +6,7 @@
 </div>
 <div class="container">
 	<div class="row">
-		<form role="form" method="post" action="{{ route('admin.challenges.update', $challenge->id) }}">
+		<form role="form" method="patch" action="{{ route('admin.challenges.update', $challenge->id) }}">
 			{{ csrf_field() }}
 			<div class="form-group">
 				<label for="inputCategory">
