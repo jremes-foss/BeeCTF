4c995754621adb301fff872dcc1a2879d8c71606
Juha Remes <jremes@outlook.com>
2019-06-29 15:43:53 +0100

    Add inputFile field to edit.blade.php

--- resources/views/admin/challenges/edit.blade.php
+++ resources/views/admin/challenges/edit.blade.php
@@ -54,6 +54,16 @@
 					<span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span><input type="text" class="form-control" id="inputContent" name="inputContent" value="{{ $challenge->content }}">
 				</div>
 			</div>
+			<div class="form-group">
+				<label for="inputFile">
+					File
+				</label>
+				<div class="input-group Content">
+					<span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
+					<input type="file" class="form-control" id="inputFile" name="inputFile" value="{{ $challenge->resource }}">
+				</div>
+			</div>
+
 		
 		<button type="submit" class="btn btn-primary">Submit</button>
 
