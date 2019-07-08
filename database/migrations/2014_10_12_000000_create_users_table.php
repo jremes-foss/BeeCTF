16771c7b5298ff547b5372513bd29349b92ed14f
Juha Remes <jremes@outlook.com>
2019-07-08 19:56:59 +0100

    Remove score as user table column

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,7 +18,6 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
-            $table->integer('score')->nullable();
             $table->string('team')->nullable();
             $table->string('user_type')->default("User");
             $table->rememberToken();
