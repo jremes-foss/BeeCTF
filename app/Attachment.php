76aea222fd3cbdf7f3b9a258a5b6f713116cf33f
Juha Remes <jremes@outlook.com>
2019-10-03 21:57:32 +0100

    Add Attachment.php model with methods.

--- app/Attachment.php
+++ app/Attachment.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Attachment extends Model
 {
-    //
+    protected $table = 'attachments';
+
+    public function challenge() {
+    	return $this->belongsTo('App\Challenge');
+    }
 }
