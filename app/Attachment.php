44b43c309b17e5370062d31e4ee7aeaac482ae1c
Juha Remes <jremes@outlook.com>
2019-10-04 21:32:54 +0100

    Add fillable to Attachment model

--- app/Attachment.php
+++ app/Attachment.php
@@ -8,6 +8,12 @@ class Attachment extends Model
 {
     protected $table = 'attachments';
 
+    protected $fillable = [
+    	'challenge_id',
+    	'filename',
+    	'url'
+    ];
+
     public function challenge() {
     	return $this->belongsTo('App\Challenge');
     }
