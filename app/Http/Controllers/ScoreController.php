dd460601348e3de81a03589b8166a52a3cc5fc3c
Juha Remes <jremes@outlook.com>
2019-11-01 20:34:38 +0000

    Add PHPDoc for getScores and getSolveds

--- app/Http/Controllers/ScoreController.php
+++ app/Http/Controllers/ScoreController.php
@@ -8,10 +8,18 @@ use App\Score;
 
 class ScoreController extends Controller
 {
+    /**
+     * Gets the player scores.
+     *
+     */
     public function getScores() {
     	// TODO
     }
 
+    /**
+     * Gets the number of solved challenges per player.
+     *
+     */
     public function getSolveds() {
     	// TODO
     }
