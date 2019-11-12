130c28ce102e9af4f7043a5e8946eb2306a9cc6a
Juha Remes <jremes@outlook.com>
2019-11-12 14:25:20 +0000

    Build the base score table.

--- resources/views/scoreboard.blade.php
+++ resources/views/scoreboard.blade.php
@@ -1,4 +1,25 @@
 @extends('layouts.app')
 @section('content')
 
+<div class="container">
+	<div class="row">
+		<table class="table table-hover">
+		<thead>
+		    <tr>
+			  	<th scope="col">#</th>
+			  	<th scope="col">Name</th>
+			  	<th scope="col">Score</th>
+		    </tr>
+		</thead>
+		<tbody>
+			@foreach($scores as $score)
+				<th scope="row"></th>
+				<td></td>
+				<td></td>
+			@endforeach
+		</tbody>
+	</table>
+	</div>
+</div>
+
 @endsection
