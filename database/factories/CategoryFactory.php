205c68b7e7fddbf91fe3bb004bf5bb69d5c76bbe
Juha Remes <jremes@outlook.com>
2019-11-25 08:46:31 +0000

    Write factory for categories.

--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -4,6 +4,7 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Category::class, function (Faker $faker) {
     return [
-        // TODO
+        'category' => 'Test Category',
+        'description' => 'This is a test category'
     ];
 });
