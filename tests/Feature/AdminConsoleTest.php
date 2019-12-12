42cfa22d9c95e0db37ffd8cb30fceaf20c53a781
Juha Remes <jremes@outlook.com>
2019-12-12 15:43:45 +0000

    Add more coverage.

--- tests/Feature/AdminConsoleTest.php
+++ tests/Feature/AdminConsoleTest.php
@@ -36,4 +36,20 @@ class AdminConsoleTest extends TestCase
 			'email' => $email
 		]);
 	}
+
+	public function testRunAdminConsoleCommandWithInvalidEmail()
+	{
+		$email = "ThisIsNotAnEmailAddress";
+		$password = $this->faker->password(8);
+
+		$this->artisan('user:create-admin')
+			->expectsOutput("*** BeeCTF Artisan Admin Creator ***")
+			->expectsOutput("[!] This command allows you to create admin user to database.")
+			->expectsQuestion("[?] Are you sure you wish to continue?", true)
+			->expectsQuestion("[?] Enter username", 'h4cker')
+			->expectsQuestion("[?] Enter email", $email)
+			->expectsQuestion("[?] Enter password", $password)
+			->expectsOutput("[!] Administrator not created. Please see error messages below: ")
+			->assertExitCode(1);
+	}
 }
