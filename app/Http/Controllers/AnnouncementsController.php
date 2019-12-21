efcd70960d3f8b77953b6138d27d82d4d32ae0d7
Juha Remes <jremes@outlook.com>
2019-12-21 14:49:30 +0000

    Add indexAdmin() method to AnnouncementsController.

--- app/Http/Controllers/AnnouncementsController.php
+++ app/Http/Controllers/AnnouncementsController.php
@@ -3,8 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Announcement;
 
 class AnnouncementsController extends Controller
 {
-    //
+    public function indexAdmin()
+    {
+    	$announcements = Announcement::all();
+    	return view('admin.announcements', compact('announcements'));
+    }
 }
