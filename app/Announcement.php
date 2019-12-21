04af60f73011255e3a1173273cdd75ee210cb2d0
Juha Remes <jremes@outlook.com>
2019-12-21 01:52:46 +0000

    Add table and fillable.

--- app/Announcement.php
+++ app/Announcement.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Announcement extends Model
 {
-    //
+    protected $table = "announcements";
+
+    protected $fillable = [
+    	'title',
+    	'content'
+    ]; 
 }
