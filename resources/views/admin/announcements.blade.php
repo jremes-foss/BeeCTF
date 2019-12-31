312df34acef26d042c321e7bed6b31fbe23b395c
Juha Remes <jremes@outlook.com>
2019-12-31 14:08:55 +0000

    Add thead to announcements.blade.php

--- resources/views/admin/announcements.blade.php
+++ resources/views/admin/announcements.blade.php
@@ -10,6 +10,11 @@
 	<a class="btn btn-primary" href="{{ route('admin.categories.create') }}" role="button" style="float: right;">New Category</a>
 	<table class="table table-hover">
 		<thead>
+			<tr>
+				<th scope="col">#</th>
+				<th scope="col">Title</th>
+				<th scope="col">Content</th>
+			</tr>
 		</thead>
 		<tbody>
 		</tbody>
