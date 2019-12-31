108b0351dd223582d2ef741c156bacc9e2a0b845
Juha Remes <jremes@outlook.com>
2019-12-31 15:34:30 +0000

    Initialize AnnouncementsTest.php

---
+++ tests/Unit/AnnouncementsTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class AnnouncementsTest extends TestCase 
+{
+	// TODO
+}
