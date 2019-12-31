12910ffc13806a6e91f515017af634e28d2a5086
Juha Remes <jremes@outlook.com>
2019-12-31 14:27:45 +0000

    Add create.blade.php

---
+++ resources/views/admin/announcements/create.blade.php
@@ -0,0 +1,30 @@
+@extends('layouts.app')
+@section('content')
+<div id="sidebar" class="col-md-4">
+	@include('layouts.sidebar')
+</div>
+<div class="container">
+	<div class="row">
+		<form role="form" method="post" action="{{ route('admin.announcements.store') }}">
+			{{ csrf_field() }}
+			<div class="form-group">
+				<label for="inputTitle">
+					Title
+				</label>
+				<div class="input-group Announcement">
+					<span class="input-group-addon"><i class="fa fa-address-book"></i></span>
+					<input type="text" class="form-control" id="inputTitle" name="inputTitle" placeholder="Enter Title">
+				</div>
+				<label for="inputContent">
+					Content
+				</label>
+				<div class="input-group Announcement">
+					<span class="input-group-addon"><i class="fa fa-address-book"></i></span>
+					<input type="text" class="form-control" id="inputContent" name="inputContent" placeholder="Enter Content">
+				</div>
+				<input type="submit" class="submit" value="Submit">
+			</div>
+		</form>
+	</div>
+</div>
+@endsection
