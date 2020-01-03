a9f2355860d1bda434830bf9d2e3d3884cd7b8e8
Juha Remes <jremes@outlook.com>
2020-01-03 14:29:59 +0000

    Move admin routes under is_admin middleware.

--- routes/web.php
+++ routes/web.php
@@ -26,7 +26,8 @@ Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/challenges', 'ChallengesController@indexUser')->name('user.challenges');
 Route::post('/challenges', 'ChallengesController@submitFlag')->name('user.submitflag');
 
-Route::get('/challenges/{id}/download', 'ChallengesController@download')->name('user.download');
+Route::get('/challenges/{id}/download', 'ChallengesController@download')
+    ->name('user.download');
 
 Route::get('/scoreboard', 'ScoreController@getScores')->name('scoreboard');
 
@@ -35,115 +36,105 @@ Route::get('/scontent', 'StaticContentController@index')->name('scontent');
 Route::get('/announcements', 'AnnouncementsController@indexUser')
     ->name('announcements');
 
-/*  ALL admin routes need to be grouped under isAdmin middleware!! 
-    Something like this:
-    --
-
-    Route::group(['middleware' => ['isadmin', 'auth']], function() {
-        // TODO
-    });
-
-    Look at the examples. 
-*/
 /** Admin Routes **/
 
-Route::get('/admin', 'HomeController@admin')
-	->middleware('is_admin')
-	->name('admin');
+Route::group(['middleware' => ['is_admin', 'auth']], function() {
+    Route::get('/admin', 'HomeController@admin')
+        ->middleware('is_admin')
+        ->name('admin');
+    Route::get('admin/challenges', 'ChallengesController@indexAdmin', function() {
+        return view('admin.challenges');
+    })->name('admin.challenges');
+    Route::get('admin/challenges/create', 'ChallengesController@create', function() {
+        return view('admin.challenges.create');
+    })->name('admin.challenges.create');
 
-/** Challenges */
+    Route::post('admin/challenges/create', [
+        'uses' => 'ChallengesController@store'
+    ])->name('admin.challenges.store');
 
-Route::get('admin/challenges', 'ChallengesController@indexAdmin', function() {
-    return view('admin.challenges');
-})->name('admin.challenges');
+    Route::get('admin/challenges/{id}/edit', 'ChallengesController@edit', function() {
+        return view('admin.challenges.edit');
+    })->name('admin.challenges.edit');
 
-Route::get('admin/challenges/create', 'ChallengesController@create', function() {
-    return view('admin.challenges.create');
-})->name('admin.challenges.create');
+    Route::post('admin/challenges/{id}/update', [
+        'uses' => 'ChallengesController@update'
+    ])->name('admin.challenges.update');
 
-Route::post('admin/challenges/create', [
-    'uses' => 'ChallengesController@store'
-])->name('admin.challenges.store');
-
-Route::get('admin/challenges/{id}/edit', 'ChallengesController@edit', function() {
-    return view('admin.challenges.edit');
-})->name('admin.challenges.edit');
-
-Route::post('admin/challenges/{id}/update', [
-    'uses' => 'ChallengesController@update'
-])->name('admin.challenges.update');
-
-Route::get('admin/challenges/{id}/delete', [
-    'uses' => 'ChallengesController@destroy'
-])->name('admin.challenges.delete');
+    Route::get('admin/challenges/{id}/delete', [
+        'uses' => 'ChallengesController@destroy'
+    ])->name('admin.challenges.delete');
 
 
-/** Categories */
+    /** Categories */
 
-Route::get('admin/categories', 'CategoriesController@index', function() {
-    return view('admin.categories');
-})->name('admin.categories');
+    Route::get('admin/categories', 'CategoriesController@index', function() {
+        return view('admin.categories');
+    })->name('admin.categories');
 
-Route::get('admin/categories/create', 'CategoriesController@create', function() {
-    return view('admin.categories.create');
-})->name('admin.categories.create');
+    Route::get('admin/categories/create', 'CategoriesController@create', function() {
+        return view('admin.categories.create');
+    })->name('admin.categories.create');
 
-Route::post('admin/categories/create', [
-    'uses' => 'CategoriesController@store'
-])->name('admin.categories.store');
+    Route::post('admin/categories/create', [
+        'uses' => 'CategoriesController@store'
+    ])->name('admin.categories.store');
 
-Route::get('admin/categories/{id}/edit', 'CategoriesController@edit', function() {
-    return view('admin.categories.edit');
-})->name('admin.categories.edit');
+    Route::get('admin/categories/{id}/edit', 'CategoriesController@edit', function() {
+        return view('admin.categories.edit');
+    })->name('admin.categories.edit');
 
-Route::post('admin/categories/{id}/update', [
-    'uses' => 'CategoriesController@update'
-])->name('admin.categories.update');
+    Route::post('admin/categories/{id}/update', [
+        'uses' => 'CategoriesController@update'
+    ])->name('admin.categories.update');
 
-Route::get('admin/categories/{id}/delete', [
-    'uses' => 'CategoriesController@destroy'
-])->name('admin.categories.delete');
+    Route::get('admin/categories/{id}/delete', [
+        'uses' => 'CategoriesController@destroy'
+    ])->name('admin.categories.delete');
 
-/** Users */
+    /** Users */
 
-Route::get('admin/users', 'UserController@index', function() {
-    return view('admin.users');
-})->name('admin.users');
+    Route::get('admin/users', 'UserController@index', function() {
+        return view('admin.users');
+    })->name('admin.users');
 
-Route::get('admin/users/{id}/edit', [
-    'uses' => 'UserController@edit'
-])->name('admin.users.edit');
+    Route::get('admin/users/{id}/edit', [
+        'uses' => 'UserController@edit'
+    ])->name('admin.users.edit');
 
-Route::post('admin/users/{id}/update', [
-    'uses' => 'UserController@update'
-])->name('admin.users.update');
+    Route::post('admin/users/{id}/update', [
+        'uses' => 'UserController@update'
+    ])->name('admin.users.update');
 
-Route::get('admin/users/{id}/delete', [
-    'uses' => 'UserController@destroy'
-])->name('admin.users.delete');
+    Route::get('admin/users/{id}/delete', [
+        'uses' => 'UserController@destroy'
+    ])->name('admin.users.delete');
 
-/** Announcements */
+    /** Announcements */
 
-Route::get('admin/announcements', 'AnnouncementsController@indexAdmin', function() {
-    return view('admin.announcements');
-})->name('admin.announcements');
+    Route::get('admin/announcements', 'AnnouncementsController@indexAdmin', function() {
+        return view('admin.announcements');
+    })->name('admin.announcements');
 
-Route::get('admin/announcements/create', 'AnnouncementsController@create', function() {
-    return view('admin.announcements.create');
-})->name('admin.announcements.create');
+    Route::get('admin/announcements/create', 'AnnouncementsController@create', function() {
+        return view('admin.announcements.create');
+    })->name('admin.announcements.create');
 
-Route::post('admin/announcements/create', [
-    'uses' => 'AnnouncementsController@store'
-])->name('admin.announcements.store');
+    Route::post('admin/announcements/create', [
+        'uses' => 'AnnouncementsController@store'
+    ])->name('admin.announcements.store');
 
-Route::get('admin/announcements/{id}/edit', [
-    'uses' => 'AnnouncementsController@edit'
-])->name('admin.announcements.edit');
+    Route::get('admin/announcements/{id}/edit', [
+        'uses' => 'AnnouncementsController@edit'
+    ])->name('admin.announcements.edit');
 
-Route::post('admin/announcements/{id}/update', [
-    'uses' => 'AnnouncementsController@update'
-])->name('admin.announcements.update');
+    Route::post('admin/announcements/{id}/update', [
+        'uses' => 'AnnouncementsController@update'
+    ])->name('admin.announcements.update');
 
-Route::get('admin/announcements/{id}/delete', [
-    'uses' => 'AnnouncementsController@destroy'
-])->name('admin.announcements.delete');
+    Route::get('admin/announcements/{id}/delete', [
+        'uses' => 'AnnouncementsController@destroy'
+    ])->name('admin.announcements.delete');
+});
+
+
