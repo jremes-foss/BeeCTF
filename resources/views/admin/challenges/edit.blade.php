caffe16aab3609e6f5697ae9838aa8c4b65eebf7
Juha Remes <jremes@outlook.com>
2020-02-05 08:34:29 +0000

    Refactor and debug Blade template, something is still off though...

--- resources/views/admin/challenges/edit.blade.php
+++ resources/views/admin/challenges/edit.blade.php
@@ -17,7 +17,7 @@
 					<span class="input-group-addon"><i class="fa fa-address-book"></i></span>
 					<select class="form-control" id="inputCategory" name="inputCategory">
 					@foreach($categories as $category)
-						<option value="{{ $category->category }}">{{ $category->category }}</option>
+						<option value="{{ $challenge->challenge_categories->category_id }}">{{ $category->category }}</option>
 					@endforeach
 					</select>
 				</div>
