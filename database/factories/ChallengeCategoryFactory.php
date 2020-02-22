de9b6c76401a547564a9e488e0648f65acb09c68
Juha Remes <jremes@outlook.com>
2020-02-22 17:07:38 +0000

    Fix the errors with factory

--- database/factories/ChallengeCategoryFactory.php
+++ database/factories/ChallengeCategoryFactory.php
@@ -2,10 +2,9 @@
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
-use App\Model;
 use Faker\Generator as Faker;
 
-$factory->define(ChallengeCategory::class, function (Faker $faker) {
+$factory->define(App\ChallengeCategory::class, function (Faker $faker) {
     return [
         'category_id' => 1,
         'challenge_id' => 1
