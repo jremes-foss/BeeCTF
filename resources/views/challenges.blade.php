16e8e74f4141add8593eb4e845e00010175143d8
Juha Remes <jremes@outlook.com>
2020-02-24 20:46:32 +0000

    Categories are now displayed in the main challenges page.

--- resources/views/challenges.blade.php
+++ resources/views/challenges.blade.php
@@ -29,7 +29,13 @@
 					</h4>
 				</div>
 				<div class="panel-body">
-					<p>Category: {{ $challenge->category }}</p>
+					<!-- Another ugly hack here, refactor pl0x. -->
+					<p>Category: 
+						{{ $challenge->join('challenge_category', 'challenge_category.challenge_id', '=', 'challenges.id')
+						->join('categories', 'challenge_category.category_id', '=', 'categories.id')
+						->select('categories.category')->where('challenge_category.challenge_id', '=', $challenge->id)
+						->value('category') 
+					}}</p>
 					<p>Score: {{ $challenge->score }}</p>
 					<p>Description: {{ $challenge->content }}</p>
 					<p>Attachment: <a href="{{ route('user.download', $challenge->id) }}">Download</a></p>
