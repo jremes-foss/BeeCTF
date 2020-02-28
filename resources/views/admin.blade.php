7db9724057adf665745ce86e22b3d55d72e8f83d
Juha Remes <jremes@outlook.com>
2020-02-28 21:24:25 +0000

    Add nav tags around the sidebar

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -1,9 +1,11 @@
 @extends('layouts.app')
 
 @section('content')
-	<div id="sidebar" class="col-md-4">
-		@include('layouts.sidebar')
-	</div>
+	<nav id="sidebar">
+		<div id="sidebar" class="col-md-4">
+			@include('layouts.sidebar')
+		</div>
+	</nav>
 	<div class="container text-center">
 	    <h1>Admin Section</h1>
 	</div>
