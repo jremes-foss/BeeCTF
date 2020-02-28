a37ce473cfc546e8196e95c15853ad9a8091e8ed
Juha Remes <jremes@outlook.com>
2020-02-28 22:01:23 +0000

    Tinker with styles a bit, getting there...

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,10 +1,12 @@
-<div class="wrapper">
-    <nav class="sidebar-nav bg-light">
-        <ul class="nav nav-pills nav-stacked">
-            <li><a href="{{ route('admin.users') }}">Users</a></li>
-            <li><a href="{{ route('admin.categories') }}">Categories</a></li>
-            <li><a href="{{ route('admin.challenges') }}">Challenges</a></li>
-            <li><a href="{{ route('admin.announcements') }}">Announcements</a></li>
-        </ul>
-    </nav>
+<div class="wrapper container-fluid">
+	<div class="row">
+	    <nav class="col-sm-3 col-md-2 sidebar" id="navbar">
+	        <ul class="nav nav-sidebar d-inline">
+	            <li><a href="{{ route('admin.users') }}">Users</a></li>
+	            <li><a href="{{ route('admin.categories') }}">Categories</a></li>
+	            <li><a href="{{ route('admin.challenges') }}">Challenges</a></li>
+	            <li><a href="{{ route('admin.announcements') }}">Announcements</a></li>
+	        </ul>
+	    </nav>
+	</div>
 </div>
