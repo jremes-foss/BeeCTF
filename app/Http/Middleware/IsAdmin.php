b72701af828712d4d31a0bee53fb25d339b57669
Juha Remes <jremes@outlook.com>
2020-03-23 21:36:36 +0000

    Clean IsAdmin.php for PSR2

--- app/Http/Middleware/IsAdmin.php
+++ app/Http/Middleware/IsAdmin.php
@@ -15,7 +15,7 @@ class IsAdmin
      */
     public function handle($request, Closure $next)
     {
-        if(auth()->user()->isAdmin()) {
+        if (auth()->user()->isAdmin()) {
             return $next($request);
         }
 
