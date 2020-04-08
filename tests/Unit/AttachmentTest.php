d67bed9eb6647844625f9b4a82c09d75204dc8af
Juha Remes <jremes@outlook.com>
2020-04-08 09:06:02 +0100

    Reformat AttachmentTest.php

--- tests/Unit/AttachmentTest.php
+++ tests/Unit/AttachmentTest.php
@@ -10,24 +10,24 @@ use App\Challenge;
 
 class AttachmentTest extends TestCase
 {
-	use RefreshDatabase;
+    use RefreshDatabase;
 
-	public function testAttachmentRelationship()
-	{
-		$attachment = factory(Attachment::class)->create([
-			'challenge_id' => 1,
-			'filename' => 'test.zip',
-			'url' => 'http://127.0.0.1'
-		]);
+    public function testAttachmentRelationship()
+    {
+        $attachment = factory(Attachment::class)->create([
+            'challenge_id' => 1,
+            'filename' => 'test.zip',
+            'url' => 'http://127.0.0.1'
+        ]);
 
-		factory(\App\Challenge::class)->create([
-			'score' => '250',
-			'title' => 'TEST',
-			'flag' => 'FLAG{th1s_1s_4_t3stSt}',
-			'content' => 'This is a test.'
-		]);
+        factory(\App\Challenge::class)->create([
+            'score' => '250',
+            'title' => 'TEST',
+            'flag' => 'FLAG{th1s_1s_4_t3stSt}',
+            'content' => 'This is a test.'
+        ]);
 
-		$challenge = $attachment->challenge;
-		$this->assertEquals('TEST', $challenge->title);
-	}
+        $challenge = $attachment->challenge;
+        $this->assertEquals('TEST', $challenge->title);
+    }
 }
