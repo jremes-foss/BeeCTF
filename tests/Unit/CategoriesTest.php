dd5dd472578904be4faa9402ed578c7f048b84c3
Juha Remes <jremes@outlook.com>
2020-04-09 08:50:50 +0100

    Reformat CategoriesTest.php

--- tests/Unit/CategoriesTest.php
+++ tests/Unit/CategoriesTest.php
@@ -10,118 +10,118 @@ use App\Challenge;
 
 class CategoriesTest extends TestCase
 {
-	use RefreshDatabase;
+    use RefreshDatabase;
 
-	public function testChallengesRelationship()
-	{
-		$challenge = factory(Challenge::class)->make([
-			'category' => 'Crypto'
-		]);
+    public function testChallengesRelationship()
+    {
+        $challenge = factory(Challenge::class)->make([
+            'category' => 'Crypto'
+        ]);
 
-		$category = $challenge->category;
+        $category = $challenge->category;
 
-		$this->assertEquals('Crypto', $category);
-	}
+        $this->assertEquals('Crypto', $category);
+    }
 
-	public function testHasMany()
-	{
-		factory(\App\Challenge::class)->create([
-			'score' => '250',
-			'title' => 'TEST',
-			'flag' => 'FLAG{th1s_1s_4_t3stSt}',
-			'content' => 'This is a test.'
-		]);
+    public function testHasMany()
+    {
+        factory(\App\Challenge::class)->create([
+            'score' => '250',
+            'title' => 'TEST',
+            'flag' => 'FLAG{th1s_1s_4_t3stSt}',
+            'content' => 'This is a test.'
+        ]);
 
-		factory(\App\Category::class)->create([
-	        'category' => 'Test Category',
-    	    'description' => 'This is a test category'
-		]);
+        factory(\App\Category::class)->create([
+            'category' => 'Test Category',
+            'description' => 'This is a test category'
+        ]);
 
-		$category = new Category();
-		$challenges = $category->challenges();
-		$this->assertEquals('object', gettype($challenges));
-	}
+        $category = new Category();
+        $challenges = $category->challenges();
+        $this->assertEquals('object', gettype($challenges));
+    }
 
-	public function testIndex() 
-	{
-		$admin = factory(\App\User::class)
-			->states('admin')
-			->create();
-		$response = $this->actingAs($admin)->get('admin/categories');
-		$response->assertStatus(200);
-	}
+    public function testIndex()
+    {
+        $admin = factory(\App\User::class)
+            ->states('admin')
+            ->create();
+        $response = $this->actingAs($admin)->get('admin/categories');
+        $response->assertStatus(200);
+    }
 
-	public function testCreate()
-	{
-		$admin = factory(\App\User::class)
-			->states('admin')
-			->create();
-		$response = $this->actingAs($admin)->get('admin/categories/create');
-		$response->assertStatus(200);
-	}
+    public function testCreate()
+    {
+        $admin = factory(\App\User::class)
+            ->states('admin')
+            ->create();
+        $response = $this->actingAs($admin)->get('admin/categories/create');
+        $response->assertStatus(200);
+    }
 
-	public function testStore()
-	{
-		$admin = factory(\App\User::class)
-			->states('admin')
-			->create();
+    public function testStore()
+    {
+        $admin = factory(\App\User::class)
+            ->states('admin')
+            ->create();
 
-		$data = [
-			'inputCategory' => 'Crypto',
-			'inputDescription' => 'Cryptography-related challenges'
-		];
+        $data = [
+            'inputCategory' => 'Crypto',
+            'inputDescription' => 'Cryptography-related challenges'
+        ];
 
-		$response = $this->actingAs($admin)->post('admin/categories/create', $data);
-		$response->assertStatus(302);
-	}
+        $response = $this->actingAs($admin)->post('admin/categories/create', $data);
+        $response->assertStatus(302);
+    }
 
-	public function testEdit() 
-	{
-		$admin = factory(\App\User::class)
-			->states('admin')
-			->create();
+    public function testEdit()
+    {
+        $admin = factory(\App\User::class)
+            ->states('admin')
+            ->create();
 
-		factory(\App\Category::class)->create([
-	        'category' => 'Test Category',
-    	    'description' => 'This is a test category'
-		]);
+        factory(\App\Category::class)->create([
+            'category' => 'Test Category',
+            'description' => 'This is a test category'
+        ]);
 
-		$response = $this->actingAs($admin)->get('admin/categories/1/edit');
-		$response->assertStatus(200);
-	}
+        $response = $this->actingAs($admin)->get('admin/categories/1/edit');
+        $response->assertStatus(200);
+    }
 
-	public function testUpdate() 
-	{
-		$admin = factory(\App\User::class)
-			->states('admin')
-			->create();
+    public function testUpdate()
+    {
+        $admin = factory(\App\User::class)
+            ->states('admin')
+            ->create();
 
-		factory(\App\Category::class)->create([
-	        'category' => 'Test Category',
-    	    'description' => 'This is a test category'
-		]);
+        factory(\App\Category::class)->create([
+            'category' => 'Test Category',
+            'description' => 'This is a test category'
+        ]);
 
-		$data = [
-			'inputCategory' => 'Steganography',
-			'inputDescription' => 'This is updated test category.'
-		];
+        $data = [
+            'inputCategory' => 'Steganography',
+            'inputDescription' => 'This is updated test category.'
+        ];
 
-		$response = $this->actingAs($admin)->post('admin/categories/1/update', $data);
-		$response->assertStatus(302);
-	}
+        $response = $this->actingAs($admin)->post('admin/categories/1/update', $data);
+        $response->assertStatus(302);
+    }
 
-	public function testDestroy()
-	{
-		$admin = factory(\App\User::class)
-			->states('admin')
-			->create();
+    public function testDestroy()
+    {
+        $admin = factory(\App\User::class)
+            ->states('admin')
+            ->create();
 
-		factory(\App\Category::class)->create([
-	        'category' => 'Test Category',
-    	    'description' => 'This is a test category'
-		]);
+        factory(\App\Category::class)->create([
+            'category' => 'Test Category',
+            'description' => 'This is a test category'
+        ]);
 
-		$response = $this->actingAs($admin)->get('admin/categories/1/delete');
-		$response->assertStatus(302);
-	}
+        $response = $this->actingAs($admin)->get('admin/categories/1/delete');
+        $response->assertStatus(302);
+    }
 }
