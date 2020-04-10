6f7590c9855354a5f5c1700059c42dd53ff644ea
Juha Remes <jremes@outlook.com>
2020-04-10 11:38:17 +0100

    Reformat ChallengeCategoriesTest.php

--- tests/Unit/ChallengeCategoryTest.php
+++ tests/Unit/ChallengeCategoryTest.php
@@ -11,40 +11,39 @@ use App\ChallengeCategory;
 
 class ChallengeCategoryTest extends TestCase
 {
-	use RefreshDatabase;
+    use RefreshDatabase;
 
-	public function testChallengeCategoryRelationship()
-	{
-		$challenge = factory(Challenge::class)->create([
-			'score' => '250',
-			'title' => 'TEST',
-			'flag' => 'FLAG{th1s_1s_4_t3st}',
-			'content' => 'This is a test.'
-		]);
+    public function testChallengeCategoryRelationship()
+    {
+        $challenge = factory(Challenge::class)->create([
+            'score' => '250',
+            'title' => 'TEST',
+            'flag' => 'FLAG{th1s_1s_4_t3st}',
+            'content' => 'This is a test.'
+        ]);
 
-		$category = factory(Category::class)->create([
-			'category' => 'Crypto'
-		]);
+        $category = factory(Category::class)->create([
+            'category' => 'Crypto'
+        ]);
 
-		$challenge_category = factory(ChallengeCategory::class)->create([
-			'category_id' => 1,
-			'challenge_id' => 1
-		]);
+        $challenge_category = factory(ChallengeCategory::class)->create([
+            'category_id' => 1,
+            'challenge_id' => 1
+        ]);
 
-		$challenge = Challenge::find(1);
-		$challenge_category = ChallengeCategory::find(1);
+        $challenge = Challenge::find(1);
+        $challenge_category = ChallengeCategory::find(1);
 
-		$this->assertEquals(1, $challenge->challengeCategories->challenge_id);
-		$this->assertEquals(1, $challenge->challengeCategories->category_id);
-		
-		// Test categories relationship
-		$this->assertEquals('Crypto', $challenge->challengeCategories->categories->category);
+        $this->assertEquals(1, $challenge->challengeCategories->challenge_id);
+        $this->assertEquals(1, $challenge->challengeCategories->category_id);
 
-		// Test challenges relationship
-		$this->assertEquals(250, $challenge_category->challenges->score);
-		$this->assertEquals('TEST', $challenge_category->challenges->title);
-		$this->assertEquals('FLAG{th1s_1s_4_t3st}', $challenge_category->challenges->flag);
-		$this->assertEquals('This is a test.', $challenge_category->challenges->content);
+        // Test categories relationship
+        $this->assertEquals('Crypto', $challenge->challengeCategories->categories->category);
 
-	}
+        // Test challenges relationship
+        $this->assertEquals(250, $challenge_category->challenges->score);
+        $this->assertEquals('TEST', $challenge_category->challenges->title);
+        $this->assertEquals('FLAG{th1s_1s_4_t3st}', $challenge_category->challenges->flag);
+        $this->assertEquals('This is a test.', $challenge_category->challenges->content);
+    }
 }
