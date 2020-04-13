e91eb060c303f9561e7bf800ff87b84a3ae6827d
Juha Remes <jremes@outlook.com>
2020-04-13 19:26:14 +0100

    Reformat StaticContentTest.php

--- tests/Unit/StaticContentTest.php
+++ tests/Unit/StaticContentTest.php
@@ -9,12 +9,12 @@ use App\User;
 
 class StaticContentTest extends TestCase
 {
-	use RefreshDatabase;
-	
-	public function testStaticContentIndex()
-	{
-		$user = factory(\App\User::class)->create();
-		$response = $this->actingAs($user)->get('/scontent');
-		$response->assertStatus(200);
-	}
+    use RefreshDatabase;
+    
+    public function testStaticContentIndex()
+    {
+        $user = factory(\App\User::class)->create();
+        $response = $this->actingAs($user)->get('/scontent');
+        $response->assertStatus(200);
+    }
 }
