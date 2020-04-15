4c2490774bcf5d5606e2027cdf7ba860dce8a2b6
Juha Remes <jremes@outlook.com>
2020-04-15 08:32:36 +0100

    Reformat AdminConsoleTest.php

--- tests/Feature/AdminConsoleTest.php
+++ tests/Feature/AdminConsoleTest.php
@@ -9,47 +9,47 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class AdminConsoleTest extends TestCase
 {
-	use WithFaker;
-	use RefreshDatabase;
+    use WithFaker;
+    use RefreshDatabase;
 
-	public function testAdminConsoleCommandExists()
-	{
-		$this->assertTrue(class_exists(\App\Console\Commands\CreateAdministrator::class));
-	}
+    public function testAdminConsoleCommandExists()
+    {
+        $this->assertTrue(class_exists(\App\Console\Commands\CreateAdministrator::class));
+    }
 
-	public function testRunAdminConsoleCommand() 
-	{
-		$email = $this->faker->email;
-		$password = $this->faker->password(8);
+    public function testRunAdminConsoleCommand() 
+    {
+        $email = $this->faker->email;
+        $password = $this->faker->password(8);
 
-		$this->artisan('user:create-admin')
-			->expectsOutput("*** BeeCTF Artisan Admin Creator ***")
-			->expectsOutput("[!] This command allows you to create admin user to database.")
-			->expectsQuestion("[?] Are you sure you wish to continue?", true)
-			->expectsQuestion("[?] Enter username", 'h4cker')
-			->expectsQuestion("[?] Enter email", $email)
-			->expectsQuestion("[?] Enter password", $password)
-			->expectsOutput("[!] Administrative user has been created.")
-			->assertExitCode(0);
+        $this->artisan('user:create-admin')
+            ->expectsOutput("*** BeeCTF Artisan Admin Creator ***")
+            ->expectsOutput("[!] This command allows you to create admin user to database.")
+            ->expectsQuestion("[?] Are you sure you wish to continue?", true)
+            ->expectsQuestion("[?] Enter username", 'h4cker')
+            ->expectsQuestion("[?] Enter email", $email)
+            ->expectsQuestion("[?] Enter password", $password)
+            ->expectsOutput("[!] Administrative user has been created.")
+            ->assertExitCode(0);
 
-		$this->assertDatabaseHas('users', [
-			'email' => $email
-		]);
-	}
+        $this->assertDatabaseHas('users', [
+            'email' => $email
+        ]);
+    }
 
-	public function testRunAdminConsoleCommandWithInvalidEmail()
-	{
-		$email = "ThisIsNotAnEmailAddress";
-		$password = $this->faker->password(8);
+    public function testRunAdminConsoleCommandWithInvalidEmail()
+    {
+        $email = "ThisIsNotAnEmailAddress";
+        $password = $this->faker->password(8);
 
-		$this->artisan('user:create-admin')
-			->expectsOutput("*** BeeCTF Artisan Admin Creator ***")
-			->expectsOutput("[!] This command allows you to create admin user to database.")
-			->expectsQuestion("[?] Are you sure you wish to continue?", true)
-			->expectsQuestion("[?] Enter username", 'h4cker')
-			->expectsQuestion("[?] Enter email", $email)
-			->expectsQuestion("[?] Enter password", $password)
-			->expectsOutput("[!] Administrator not created. Please see error messages below: ")
-			->assertExitCode(1);
-	}
+        $this->artisan('user:create-admin')
+            ->expectsOutput("*** BeeCTF Artisan Admin Creator ***")
+            ->expectsOutput("[!] This command allows you to create admin user to database.")
+            ->expectsQuestion("[?] Are you sure you wish to continue?", true)
+            ->expectsQuestion("[?] Enter username", 'h4cker')
+            ->expectsQuestion("[?] Enter email", $email)
+            ->expectsQuestion("[?] Enter password", $password)
+            ->expectsOutput("[!] Administrator not created. Please see error messages below: ")
+            ->assertExitCode(1);
+    }
 }
