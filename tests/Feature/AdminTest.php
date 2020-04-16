8474b401830b21271064e70c672ae10749f73018
Juha Remes <jremes@outlook.com>
2020-04-16 08:58:45 +0100

    Reformat AdminTest.php

--- tests/Feature/AdminTest.php
+++ tests/Feature/AdminTest.php
@@ -8,23 +8,23 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class AdminTest extends TestCase
 {
-	use RefreshDatabase;
+    use RefreshDatabase;
 
-	public function testUserCannotAccessAdmin()
-	{
-		$user = factory(\App\User::class)->create();
-		$this->actingAs($user)
-			->get('/admin')
-			->assertRedirect('home');
-	}
+    public function testUserCannotAccessAdmin()
+    {
+        $user = factory(\App\User::class)->create();
+        $this->actingAs($user)
+            ->get('/admin')
+            ->assertRedirect('home');
+    }
 
-	public function testAdminCanAccessAdmin()
-	{
-		$admin = factory(\App\User::class)
-			->states('admin')
-			->create();
-		$this->actingAs($admin)
-			->get('/admin')
-			->assertStatus(200);
-	}
+    public function testAdminCanAccessAdmin()
+    {
+        $admin = factory(\App\User::class)
+            ->states('admin')
+            ->create();
+        $this->actingAs($admin)
+            ->get('/admin')
+            ->assertStatus(200);
+    }
 }
