48c2e843813e31497902c6809061f3dd0ddea3dc
Juha Remes <jremes@outlook.com>
2020-04-17 08:34:39 +0100

    Reformat ChallengesTest.php

--- tests/Feature/ChallengesTest.php
+++ tests/Feature/ChallengesTest.php
@@ -7,29 +7,29 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class ChallengesTest extends TestCase {
 
-	use RefreshDatabase;
+    use RefreshDatabase;
 
-	public function testAdminSidebarWorks()
-	{
-		$admin = factory(\App\User::class)
-			->states('admin')
-			->create();
-		
-		$response = $this->actingAs($admin)->get('/admin');
-		$response->assertSee('/admin/challenges');
-	}
+    public function testAdminSidebarWorks()
+    {
+        $admin = factory(\App\User::class)
+            ->states('admin')
+            ->create();
+        
+        $response = $this->actingAs($admin)->get('/admin');
+        $response->assertSee('/admin/challenges');
+    }
 
-	public function testAdminChallengesClickThrough()
-	{
-		$admin = factory(\App\User::class)
-			->states('admin')
-			->create();
-		
-		$response = $this->actingAs($admin)->get('/admin/challenges');
-		$response->assertSee('Category');
-		$response->assertSee('Score');
-		$response->assertSee('Title');
-		$response->assertSee('Flag');
-		$response->assertSee('Content');
-	}
+    public function testAdminChallengesClickThrough()
+    {
+        $admin = factory(\App\User::class)
+            ->states('admin')
+            ->create();
+        
+        $response = $this->actingAs($admin)->get('/admin/challenges');
+        $response->assertSee('Category');
+        $response->assertSee('Score');
+        $response->assertSee('Title');
+        $response->assertSee('Flag');
+        $response->assertSee('Content');
+    }
 }
