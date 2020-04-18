bd375cbe212a8841e1310fbe29ab89c1c856a112
Juha Remes <jremes@outlook.com>
2020-04-18 14:20:36 +0100

    Reformat PasswordResetTest.php

--- tests/Feature/PasswordResetTest.php
+++ tests/Feature/PasswordResetTest.php
@@ -12,9 +12,9 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class PasswordResetTest extends TestCase
 {
-	use RefreshDatabase;
+    use RefreshDatabase;
 
-	public function testPasswordResetRequestForm()
+    public function testPasswordResetRequestForm()
     {
         $response = $this->get('password/reset');
         $response->assertStatus(200);
@@ -22,29 +22,29 @@ class PasswordResetTest extends TestCase
 
     public function testSendsPasswordResetEmail()
     {
-		$user = factory(User::class)->create();
-		$this->expectsNotification($user, ResetPassword::class);
-		$response = $this->post('password/email', ['email' => $user->email]);
-		$response->assertStatus(302);
+        $user = factory(User::class)->create();
+        $this->expectsNotification($user, ResetPassword::class);
+        $response = $this->post('password/email', ['email' => $user->email]);
+        $response->assertStatus(302);
     }
 
-	public function testDoesNotSendPasswordResetEmail()
-	{
-		$this->doesntExpectJobs(ResetPassword::class);
-		$this->post('password/email', ['email' => 'invalid@email.com']);
-	}
+    public function testDoesNotSendPasswordResetEmail()
+    {
+        $this->doesntExpectJobs(ResetPassword::class);
+        $this->post('password/email', ['email' => 'invalid@email.com']);
+    }
 
-	public function testChangesUsersPassword()
-	{
-		$user = factory(User::class)->create();
-		$token = Password::createToken($user);
-		$response = $this->post('/password/reset', [
-			'token' => $token,
-			'email' => $user->email,
-			'password' => 'p4ssw0rd',
-			'password_confirmation' => 'p4ssw0rd'
-		]);
-		
-		$this->assertTrue(Hash::check('p4ssw0rd', $user->fresh()->password));
-	}
+    public function testChangesUsersPassword()
+    {
+        $user = factory(User::class)->create();
+        $token = Password::createToken($user);
+        $response = $this->post('/password/reset', [
+            'token' => $token,
+            'email' => $user->email,
+            'password' => 'p4ssw0rd',
+            'password_confirmation' => 'p4ssw0rd'
+        ]);
+
+        $this->assertTrue(Hash::check('p4ssw0rd', $user->fresh()->password));
+    }
 }
