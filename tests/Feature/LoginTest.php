17b5a852364897ae5d80d80c5e63adea51c0930a
Juha Remes <jremes@outlook.com>
2020-04-19 13:35:40 +0100

    PSR2 fixes: LoginTest.php

--- tests/Feature/LoginTest.php
+++ tests/Feature/LoginTest.php
@@ -9,63 +9,63 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class LoginTest extends TestCase
 {
-	use RefreshDatabase;
+    use RefreshDatabase;
 
-	public function testShowMainPage()
-	{
-		$response = $this->get('/home');
-		$response->assertStatus(302);
-	}
+    public function testShowMainPage()
+    {
+        $response = $this->get('/home');
+        $response->assertStatus(302);
+    }
 
-	public function testLoginFormDisplayed()
-	{
-		$response = $this->get('/login');
-		$response->assertStatus(200);
-	}
+    public function testLoginFormDisplayed()
+    {
+        $response = $this->get('/login');
+        $response->assertStatus(200);
+    }
 
-	public function testLoginWithValidUser()
-	{
-		$user = factory(User::class)->create();
-		$response = $this->post('/login', [
-			'email' => $user->email,
-			'password' => 'secret'
-		]);
+    public function testLoginWithValidUser()
+    {
+        $user = factory(User::class)->create();
+        $response = $this->post('/login', [
+            'email' => $user->email,
+            'password' => 'secret'
+        ]);
 
-		$response->assertStatus(302);
-		$this->assertAuthenticatedAs($user);
-	}
+        $response->assertStatus(302);
+        $this->assertAuthenticatedAs($user);
+    }
 
-	public function testInvalidUser()
-	{
-		$invalidUser = factory(User::class)->create();
+    public function testInvalidUser()
+    {
+        $invalidUser = factory(User::class)->create();
 
-		$response = $this->post('/login', [
-			'email' => $invalidUser->email,
-			'password' => 'th1s1s1nv4l1dp4ssw0rd!'
-		]);
+        $response = $this->post('/login', [
+            'email' => $invalidUser->email,
+            'password' => 'th1s1s1nv4l1dp4ssw0rd!'
+        ]);
 
-		$response->assertSessionHasErrors();
-		$this->assertGuest();
-	}
+        $response->assertSessionHasErrors();
+        $this->assertGuest();
+    }
 
-	public function testLogout()
-	{
-		$user = factory(User::class)->create();
-		$response = $this->actingAs($user)->post('/logout');
-		$response->assertStatus(302);
-		$this->assertGuest();
-	}
+    public function testLogout()
+    {
+        $user = factory(User::class)->create();
+        $response = $this->actingAs($user)->post('/logout');
+        $response->assertStatus(302);
+        $this->assertGuest();
+    }
 
-	/**
-	 *	Tests that user is redirected to home page after login.
-	 */
-	public function testHome()
-	{
-		$user = factory(User::class)->create();
-		$response = $this->followingRedirects()->post('/login', [
-			'email' => $user->email,
-			'password' => 'secret'
-		]);
-		$response->assertStatus(200);
-	}
+    /**
+     *	Tests that user is redirected to home page after login.
+     */
+    public function testHome()
+    {
+        $user = factory(User::class)->create();
+        $response = $this->followingRedirects()->post('/login', [
+            'email' => $user->email,
+            'password' => 'secret'
+        ]);
+        $response->assertStatus(200);
+    }
 }
