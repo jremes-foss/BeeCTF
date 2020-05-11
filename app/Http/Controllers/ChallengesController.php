8c99b7ab47a642540adc53ecf7d226847be55af2
Juha Remes <jremes@outlook.com>
2020-05-11 20:12:24 +0100

    Bug fixes

--- app/Http/Controllers/ChallengesController.php
+++ app/Http/Controllers/ChallengesController.php
@@ -103,8 +103,8 @@ class ChallengesController extends Controller
     {
         $user_id = Auth::user()->id;
         $solved = Solved::where('user_id', $user_id)->get();
-        $challenges = Challenge::whereNotIn('id', $solved)->with('challenge_categories')->get();
-        $categories = Category::with('challenge_categories')->get();
+        $challenges = Challenge::whereNotIn('id', $solved)->with('challengeCategories')->get();
+        $categories = Category::with('challengeCategories')->get();
         return view('challenges')
             ->with('challenges', $challenges)
             ->with('categories', $categories);
