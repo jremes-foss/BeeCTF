a2752355677e915e1a1d245fb0b482cb2b80a959
Juha Remes <jremes@outlook.com>
2020-06-05 21:11:10 +0100

    Wrap the rules to row

--- resources/views/scontent.blade.php
+++ resources/views/scontent.blade.php
@@ -1,16 +1,26 @@
 @extends('layouts.app')
 @section('content')
 
-<div class="container">
-	<div class="panel panel-primary">
-		<div class="panel-heading">
-			<h4 class="panel-title">Rules</h4>
-		</div>
-		<div class="panel-body">
-			<p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sagittis tempus convallis. Suspendisse blandit non est porta rutrum. Integer quis quam justo. Nam accumsan odio id erat mollis placerat. Pellentesque in imperdiet sem, vel aliquet purus. Etiam viverra luctus sagittis. Sed hendrerit in nibh blandit pellentesque. Curabitur bibendum nunc quis ipsum condimentum gravida. Curabitur fermentum luctus commodo. Sed porta mauris eget lacus ultricies, in ultricies tellus bibendum. Morbi vestibulum ipsum nec ultricies tempus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Interdum et malesuada fames ac ante ipsum primis in faucibus.</p>
-			<p class="card-text">Maecenas auctor suscipit facilisis. Proin eleifend, sapien a placerat pellentesque, nulla est pellentesque orci, nec rhoncus lorem ex id leo. Nulla sed ultrices ligula, a efficitur tellus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque eu massa malesuada, feugiat enim ut, sagittis ipsum. Nullam non nibh nibh. Cras arcu sem, interdum et vestibulum dignissim, luctus et lacus. Duis volutpat ante sem. Cras a tortor nunc. Curabitur pharetra nibh et augue convallis, at scelerisque purus lacinia. Pellentesque interdum, mauris sed consectetur tristique, nisi metus scelerisque est, quis imperdiet diam felis vel augue. Cras at nibh iaculis, commodo lectus et, tincidunt augue. Quisque in cursus ligula, sed iaculis ipsum. Nulla laoreet viverra luctus. Integer vitae velit metus.</p>
+<div class="container rules">
+	<div class="row">
+		<div class="panel panel-primary">
+			<div class="panel-heading">
+				<h4 class="panel-title">Rules</h4>
+			</div>
+			<div class="panel-body">
+				<p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sagittis tempus convallis. Suspendisse blandit non est porta rutrum. Integer quis quam justo. Nam accumsan odio id erat mollis placerat. Pellentesque in imperdiet sem, vel aliquet purus. Etiam viverra luctus sagittis. Sed hendrerit in nibh blandit pellentesque. Curabitur bibendum nunc quis ipsum condimentum gravida. Curabitur fermentum luctus commodo. Sed porta mauris eget lacus ultricies, in ultricies tellus bibendum. Morbi vestibulum ipsum nec ultricies tempus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Interdum et malesuada fames ac ante ipsum primis in faucibus.</p>
+				<p class="card-text">Maecenas auctor suscipit facilisis. Proin eleifend, sapien a placerat pellentesque, nulla est pellentesque orci, nec rhoncus lorem ex id leo. Nulla sed ultrices ligula, a efficitur tellus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque eu massa malesuada, feugiat enim ut, sagittis ipsum. Nullam non nibh nibh. Cras arcu sem, interdum et vestibulum dignissim, luctus et lacus. Duis volutpat ante sem. Cras a tortor nunc. Curabitur pharetra nibh et augue convallis, at scelerisque purus lacinia. Pellentesque interdum, mauris sed consectetur tristique, nisi metus scelerisque est, quis imperdiet diam felis vel augue. Cras at nibh iaculis, commodo lectus et, tincidunt augue. Quisque in cursus ligula, sed iaculis ipsum. Nulla laoreet viverra luctus. Integer vitae velit metus.</p>
+			</div>
 		</div>
 	</div>
 </div>
 
 @endsection
+@push('styles')
+<style>
+	.rules {
+		text-align: center;
+	}
+</style>
+@endpush
+@stack('styles')
