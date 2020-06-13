e11686e2488aaaaba893e36bc7067f473d18e3ad
Juha Remes <jremes@outlook.com>
2020-06-13 14:54:46 +0100

    Fix the crash on empty category

--- resources/views/challenges.blade.php
+++ resources/views/challenges.blade.php
@@ -20,7 +20,7 @@
 			</div>
 		</form>
 	</div>
-	@if(request()->has('category'))
+	@if(request()->has('category') && count($challenges) > 0)
 		@foreach($challenges as $challenge)
 			<!-- Most likely not the most efficient method to do this, but it works! :) -->
 			<!-- I probably should write a scope for this. -->
@@ -58,42 +58,51 @@
 			@endforeach
 		</div>
 
-<!-- Submit Flag Modal -->
-<div class="modal fade" id="flagValidation" tabindex="-1" role="dialog" aria-labelledby="flagValidationLabel">
-	<div class="modal-dialog" role="document">
-		<div class="modal-content">
-			<div class="modal-header">
-				<h5 class="modal-title" id="exampleModalLabel">Submit Flag</h5>
-				<button type="button" class="close" data-dismiss="modal" aria-label="Cancel">
-					<span aria-hidden="true">&times;</span>
-				</button>
-			</div>
-			<div class="modal-body">
-				<p><strong>Before submission:</strong> please make sure your flag does not have any typos.</p>
-				<form id="submitFlag" method="post" action="{{ route('user.submitflag') }}">
-					<div class="form-group">
-						<input type="hidden" name="_token" value="{{ csrf_token() }}">
-						<label for="flag">Flag:</label>
-						<input type="text" name="flag" placeholder="FLAG{th1s_1s_4n_3x4mpl3}">
-						<input name="id" value="{{ $challenge->id }}" type="hidden">
-						<input type="submit" class="submit_flag" value="Submit Flag">
+		<!-- Submit Flag Modal -->
+		<div class="modal fade" id="flagValidation" tabindex="-1" role="dialog" aria-labelledby="flagValidationLabel">
+			<div class="modal-dialog" role="document">
+				<div class="modal-content">
+					<div class="modal-header">
+						<h5 class="modal-title" id="exampleModalLabel">Submit Flag</h5>
+						<button type="button" class="close" data-dismiss="modal" aria-label="Cancel">
+							<span aria-hidden="true">&times;</span>
+						</button>
 					</div>
-				</form>
-			</div>
-			<div class="modal-footer">
-				<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+					<div class="modal-body">
+						<p><strong>Before submission:</strong> please make sure your flag does not have any typos.</p>
+						<form id="submitFlag" method="post" action="{{ route('user.submitflag') }}">
+							<div class="form-group">
+								<input type="hidden" name="_token" value="{{ csrf_token() }}">
+								<label for="flag">Flag:</label>
+								<input type="text" name="flag" placeholder="FLAG{th1s_1s_4n_3x4mpl3}">
+								<input name="id" value="{{ $challenge->id }}" type="hidden">
+								<input type="submit" class="submit_flag" value="Submit Flag">
+							</div>
+						</form>
+					</div>
+					<div class="modal-footer">
+						<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+					</div>
+				</div>
 			</div>
 		</div>
-	</div>
-</div>
-@else
-<div class="container">
-	<div class="row" style="max-width: 90%">
-		<div class="alert alert-info">
-			Please choose a category to start. 
+	@elseif (count($challenges) == 0)
+		@if (request()->get('category') == "Select a category...")
+		<div class="container">
+			<div class="row" style="max-width: 90%">
+				<div class="alert alert-info">
+					Please choose a category to start.
+				</div>
+			</div>
 		</div>
-	</div>
-</div>
-@endif 
-
+		@else
+		<div class="container">
+			<div class="row" style="max-width: 90%">
+				<div class="alert alert-info">
+					No solvable challenges in category.
+				</div>
+			</div>
+		</div>
+		@endif
+	@endif
 @endsection
