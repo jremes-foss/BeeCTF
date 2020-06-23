29e2a2b9f5f833d79f8487756a129e147cda468e
Juha Remes <jremes@outlook.com>
2020-06-23 13:47:58 +0100

    Replace category icon

--- resources/views/admin/challenges/edit.blade.php
+++ resources/views/admin/challenges/edit.blade.php
@@ -11,7 +11,7 @@
 						Category
 					</label>
 					<div class="input-group Category">
-						<span class="input-group-addon"><i class="fa fa-address-book"></i></span>
+						<span class="input-group-addon"><i class="glyphicon glyphicon-list"></i></span>
 						<select class="form-control" id="inputCategory" name="inputCategory">
 						@foreach($categories as $category)
 							<option value="{{ $challenge_category->category_id }}">
