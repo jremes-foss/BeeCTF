a24e12653779012df460c68c6c60278eeda20585
Juha Remes <jremes@outlook.com>
2020-06-27 00:15:34 +0100

    Replace content icon

--- resources/views/admin/challenges/edit.blade.php
+++ resources/views/admin/challenges/edit.blade.php
@@ -50,7 +50,7 @@
 						Content
 					</label>
 					<div class="input-group Content">
-						<span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span><input type="text" class="form-control" id="inputContent" name="inputContent" value="{{ $challenge->content }}">
+						<span class="input-group-addon"><i class="glyphicon glyphicon-info-sign"></i></span><input type="text" class="form-control" id="inputContent" name="inputContent" value="{{ $challenge->content }}">
 					</div>
 				</div>
 				<div class="form-group">
