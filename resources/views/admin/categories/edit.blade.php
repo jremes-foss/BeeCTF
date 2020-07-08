40dc638f10a9d004ff03aeb407c02895d79d922d
Juha Remes <jremes@outlook.com>
2020-07-08 12:28:11 +0100

    Edit form: Replace category icon

--- resources/views/admin/categories/edit.blade.php
+++ resources/views/admin/categories/edit.blade.php
@@ -9,7 +9,7 @@
 					Category
 				</label>
 				<div class="input-group Category">
-					<span class="input-group-addon"><i class="fa fa-address-book"></i></span>
+					<span class="input-group-addon"><i class="glyphicon glyphicon-list"></i></span>
 					<input type="text" class="form-control" id="inputCategory" name="inputCategory" placeholder="Enter Category" value="{{ $category->category }}">
 				</div>
 				<label for="inputDescription">
