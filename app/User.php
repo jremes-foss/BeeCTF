795ec46fe17024d6ef786dc60bb0dcf13c192c59
Juha Remes <jremes@outlook.com>
2020-08-10 20:54:58 +0100

    Implement API token generation in User model

--- app/User.php
+++ app/User.php
@@ -34,4 +34,12 @@ class User extends Authenticatable
     {
         return $this->user_type === self::ADMIN_TYPE;
     }
+
+    public function generateToken()
+    {
+        $this->api_token = str_random(60);
+        $this->save();
+        
+        return $this->api_token;
+    }
 }
