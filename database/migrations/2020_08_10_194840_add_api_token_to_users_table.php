470360653e0c30625fd5644ddef150432db1c83e
Juha Remes <jremes@outlook.com>
2020-08-10 20:51:06 +0100

    Drop the column on down()

--- database/migrations/2020_08_10_194840_add_api_token_to_users_table.php
+++ database/migrations/2020_08_10_194840_add_api_token_to_users_table.php
@@ -26,7 +26,7 @@ class AddApiTokenToUsersTable extends Migration
     public function down()
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->dropColumn(['api_token']);
         });
     }
 }
