d790a93c70aa10f7890d9f335cc6102da95d58e9
Juha Remes <jremes@outlook.com>
2020-08-17 08:53:58 +0100

    Fix incorrect route

--- routes/api.php
+++ routes/api.php
@@ -17,4 +17,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('categories', 'ApiController@index');
+Route::get('categories', 'ApiController@getCategories');
