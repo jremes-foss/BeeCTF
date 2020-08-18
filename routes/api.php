8229e1ff626c34a7bc3cd6f0b0576945519af94b
Juha Remes <jremes@outlook.com>
2020-08-18 23:10:40 +0100

    Plug auth:api middleware

--- routes/api.php
+++ routes/api.php
@@ -17,4 +17,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('categories', 'ApiController@getCategories');
+Route::middleware('auth:api')->get('categories', 'ApiController@getCategories');
