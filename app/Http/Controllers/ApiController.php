62a473703d4d3f106a1097d96c9e55bbec628243
Juha Remes <jremes@outlook.com>
2020-08-19 23:33:10 +0100

    Add PHPDoc for getCategories() method

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -7,6 +7,11 @@ use App\Category;
 
 class ApiController extends Controller
 {
+    /**
+     * Return all categories in JSON format
+     *
+     * @return \Illuminate\Http\Response
+     */
     public function getCategories() 
     {
         return Category::all();
