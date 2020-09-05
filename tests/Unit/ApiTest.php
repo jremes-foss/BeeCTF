7c891c60585207dd8cb99fccf89c6938de761dea
Juha Remes <jremes@outlook.com>
2020-09-05 12:44:56 +0100

    PHPCS: Fix whitespace issue in ApiTest

--- tests/Unit/ApiTest.php
+++ tests/Unit/ApiTest.php
@@ -16,7 +16,7 @@ class ApiTest extends TestCase
         $response->assertStatus(200);
     }
 
-    public function testPingPong() 
+    public function testPingPong()
     {
         $response = $this->json('GET', 'api/ping');
         $response->assertSeeText('pong');
