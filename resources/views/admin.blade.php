4d1396ea106c11b9a6430e111395cf58db514776
Juha Remes <jremes@outlook.com>
2020-09-09 23:29:13 +0100

    admin.blade.php: tabs to spaces

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -1,8 +1,8 @@
 @extends('layouts.app')
 @section('content')
 <div class="row">
-	<div class="col-sm">
-	    <h1 class="text-center">Admin Section</h1>
-	</div>
+    <div class="col-sm">
+        <h1 class="text-center">Admin Section</h1>
+    </div>
 </div>
 @endsection
