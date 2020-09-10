f20250f1dcd0730d9d6d58317e650d5d72542e1d
Juha Remes <jremes@outlook.com>
2020-09-10 23:31:12 +0100

    announcements.blade.php: convert tabs to spaces

--- resources/views/announcements.blade.php
+++ resources/views/announcements.blade.php
@@ -2,20 +2,20 @@
 @section('content')
 
 <div class="container">
-	<div class="row" style="max-width: 90%">
-		@foreach($announcements as $announcement)
-		<div class="panel panel-primary">
-			<div class="panel-heading">
-				<h4 class="panel-title">
-					{{ $announcement->title }}
-				</h4>
-			</div>
-			<div class="panel-body">
-				<p>{{ $announcement->content }}</p>
-			</div>
-		</div>
-		@endforeach
-	</div>
+    <div class="row" style="max-width: 90%">
+        @foreach($announcements as $announcement)
+        <div class="panel panel-primary">
+            <div class="panel-heading">
+                <h4 class="panel-title">
+                    {{ $announcement->title }}
+                </h4>
+            </div>
+            <div class="panel-body">
+                <p>{{ $announcement->content }}</p>
+            </div>
+        </div>
+        @endforeach
+    </div>
 </div>
 
 @endsection
