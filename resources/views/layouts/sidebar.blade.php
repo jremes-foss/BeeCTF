f65e7a7edd590d104ffcb47eb64fb4ffd3c7d49f
Juha Remes <jremes@outlook.com>
2020-09-15 20:11:15 +0100

    sidebar.blade.php: tabs to spaces

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,10 +1,10 @@
 <nav class=" d-none d-md-block bg-light sidebar">
     <div class="sidebar-sticky">
-		<ul class="nav nav-sidebar d-inline">
-		    <li><a href="{{ route('admin.users') }}">Users</a></li>
-		    <li><a href="{{ route('admin.categories') }}">Categories</a></li>
-		    <li><a href="{{ route('admin.challenges') }}">Challenges</a></li>
-		    <li><a href="{{ route('admin.announcements') }}">Announcements</a></li>
-		</ul>
+        <ul class="nav nav-sidebar d-inline">
+            <li><a href="{{ route('admin.users') }}">Users</a></li>
+            <li><a href="{{ route('admin.categories') }}">Categories</a></li>
+            <li><a href="{{ route('admin.challenges') }}">Challenges</a></li>
+            <li><a href="{{ route('admin.announcements') }}">Announcements</a></li>
+        </ul>
     </div>
 </nav>
