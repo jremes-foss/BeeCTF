0e0f46461b44296fa2948b9a5aa4f4c1f22ee64f
Juha Remes <jremes@outlook.com>
2020-09-16 17:24:56 +0100

    announcements create.blade.php: tabs to spaces

--- resources/views/admin/announcements/create.blade.php
+++ resources/views/admin/announcements/create.blade.php
@@ -1,27 +1,27 @@
 @extends('layouts.app')
 @section('content')
 <div class="container">
-	<div class="row" style="max-width: 90%">
-		<form role="form" method="post" action="{{ route('admin.announcements.store') }}">
-			{{ csrf_field() }}
-			<div class="form-group">
-				<label for="inputTitle">
-					Title
-				</label>
-				<div class="input-group Announcement">
-					<span class="input-group-addon"><i class="glyphicon glyphicon-exclamation-sign"></i></span>
-					<input type="text" class="form-control" id="inputTitle" name="inputTitle" placeholder="Enter Title">
-				</div>
-				<label for="inputContent">
-					Content
-				</label>
-				<div class="input-group Announcement">
-					<span class="input-group-addon"><i class="glyphicon glyphicon-info-sign"></i></span>
-					<input type="text" class="form-control" id="inputContent" name="inputContent" placeholder="Enter Content">
-				</div>
-				<input type="submit" class="submit" value="Submit">
-			</div>
-		</form>
-	</div>
+    <div class="row" style="max-width: 90%">
+        <form role="form" method="post" action="{{ route('admin.announcements.store') }}">
+            {{ csrf_field() }}
+            <div class="form-group">
+                <label for="inputTitle">
+                    Title
+                </label>
+                <div class="input-group Announcement">
+                    <span class="input-group-addon"><i class="glyphicon glyphicon-exclamation-sign"></i></span>
+                    <input type="text" class="form-control" id="inputTitle" name="inputTitle" placeholder="Enter Title">
+                </div>
+                <label for="inputContent">
+                    Content
+                </label>
+                <div class="input-group Announcement">
+                    <span class="input-group-addon"><i class="glyphicon glyphicon-info-sign"></i></span>
+                    <input type="text" class="form-control" id="inputContent" name="inputContent" placeholder="Enter Content">
+                </div>
+                <input type="submit" class="submit" value="Submit">
+            </div>
+        </form>
+    </div>
 </div>
 @endsection
