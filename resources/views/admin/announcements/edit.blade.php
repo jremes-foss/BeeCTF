863bee93c23f0e15d28b148aa2804a020b3de2cc
Juha Remes <jremes@outlook.com>
2020-09-17 23:11:24 +0100

    admin announcements edit: tabs to spaces

--- resources/views/admin/announcements/edit.blade.php
+++ resources/views/admin/announcements/edit.blade.php
@@ -1,27 +1,27 @@
 @extends('layouts.app')
 @section('content')
 <div class="container">
-	<div class="row">
-		<form role="form" method="post" action="{{ route('admin.announcements.update', $announcement->id) }}">
-			{{ csrf_field() }}
-			<div class="form-group">
-				<label for="inputTitle">
-					Title
-				</label>
-				<div class="input-group Announcement">
-					<span class="input-group-addon"><i class="glyphicon glyphicon-exclamation-sign"></i></span>
-					<input type="text" class="form-control" id="inputTitle" name="inputTitle" value="{{ $announcement->title }}">
-				</div>
-				<label for="inputContent">
-					Content
-				</label>
-				<div class="input-group Announcement">
-					<span class="input-group-addon"><i class="glyphicon glyphicon-info-sign"></i></span>
-					<input type="text" class="form-control" id="inputContent" name="inputContent" value="{{ $announcement->content }}">
-				</div>
-				<input type="submit" class="submit" value="Submit">
-			</div>
-		</form>
-	</div>
+    <div class="row">
+        <form role="form" method="post" action="{{ route('admin.announcements.update', $announcement->id) }}">
+            {{ csrf_field() }}
+            <div class="form-group">
+                <label for="inputTitle">
+                    Title
+                </label>
+                <div class="input-group Announcement">
+                    <span class="input-group-addon"><i class="glyphicon glyphicon-exclamation-sign"></i></span>
+                    <input type="text" class="form-control" id="inputTitle" name="inputTitle" value="{{ $announcement->title }}">
+                </div>
+                <label for="inputContent">
+                    Content
+                </label>
+                <div class="input-group Announcement">
+                    <span class="input-group-addon"><i class="glyphicon glyphicon-info-sign"></i></span>
+                    <input type="text" class="form-control" id="inputContent" name="inputContent" value="{{ $announcement->content }}">
+                </div>
+                <input type="submit" class="submit" value="Submit">
+            </div>
+        </form>
+    </div>
 </div>
 @endsection
