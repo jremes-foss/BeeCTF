8484f063cc9b643dcb5457113d0cbbbd9f112655
Juha Remes <jremes@outlook.com>
2020-09-25 21:21:45 +0100

    categories edit.blade.php: tabs to spaces

--- resources/views/admin/categories/edit.blade.php
+++ resources/views/admin/categories/edit.blade.php
@@ -1,27 +1,27 @@
 @extends('layouts.app')
 @section('content')
 <div class="container">
-	<div class="row" style="max-width: 90%">
-		<form role="form" method="post" action="{{ route('admin.categories.update', $category->id) }}">
-			{{ csrf_field() }}
-			<div class="form-group">
-				<label for="inputCategory">
-					Category
-				</label>
-				<div class="input-group Category">
-					<span class="input-group-addon"><i class="glyphicon glyphicon-list"></i></span>
-					<input type="text" class="form-control" id="inputCategory" name="inputCategory" placeholder="Enter Category" value="{{ $category->category }}">
-				</div>
-				<label for="inputDescription">
-					Description
-				</label>
-				<div class="input-group Category">
-					<span class="input-group-addon"><i class="glyphicon glyphicon-pencil"></i></span>
-					<input type="text" class="form-control" id="inputDescription" name="inputDescription" placeholder="Enter Description" value="{{ $category->description }}">
-				</div>
-				<input type="submit" class="submit" value="Submit">
-			</div>
-		</form>
-	</div>
+    <div class="row" style="max-width: 90%">
+        <form role="form" method="post" action="{{ route('admin.categories.update', $category->id) }}">
+            {{ csrf_field() }}
+            <div class="form-group">
+                <label for="inputCategory">
+                    Category
+                </label>
+                <div class="input-group Category">
+                    <span class="input-group-addon"><i class="glyphicon glyphicon-list"></i></span>
+                    <input type="text" class="form-control" id="inputCategory" name="inputCategory" placeholder="Enter Category" value="{{ $category->category }}">
+                </div>
+                <label for="inputDescription">
+                    Description
+                </label>
+                <div class="input-group Category">
+                    <span class="input-group-addon"><i class="glyphicon glyphicon-pencil"></i></span>
+                    <input type="text" class="form-control" id="inputDescription" name="inputDescription" placeholder="Enter Description" value="{{ $category->description }}">
+                </div>
+                <input type="submit" class="submit" value="Submit">
+            </div>
+        </form>
+    </div>
 </div>
 @endsection
