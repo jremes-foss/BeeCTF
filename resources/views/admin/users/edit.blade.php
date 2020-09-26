2d2b5cec7cd6e46b91b1c6da657128f83abf10be
Juha Remes <jremes@outlook.com>
2020-09-26 15:05:56 +0100

    Admin users edit.blade.php: tabs to spaces

--- resources/views/admin/users/edit.blade.php
+++ resources/views/admin/users/edit.blade.php
@@ -3,27 +3,27 @@
 @section('content')
 
 <div class="container">
-	<div class="row" style="max-width: 90%">
-		<form role="form" method="post" action="{{ route('admin.users.update', $user->id) }}">
-			{{ method_field('post') }}
-			{{ csrf_field() }}
-			<div class="form-group">
-				<label for="inputName">
-					Name
-				</label>
-				<div class="input-group Name">
-					<span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span><input type="text" class="form-control" id="inputName" name="inputName" value="{{ $user->name }}">
-				</div>
-				<label for="inputEmail">
-					Email
-				</label>
-				<div class="input-group Email">
-					<span class="input-group-addon"><i class="glyphicon glyphicon-envelope"></i></span><input type="text" class="form-control" id="inputEmail" name="inputEmail" value="{{ $user->email }}">
-				</div>
-			</div>
-			<button type="submit" class="btn btn-primary">Submit</button>
-		</form>		
-	</div>
+    <div class="row" style="max-width: 90%">
+        <form role="form" method="post" action="{{ route('admin.users.update', $user->id) }}">
+            {{ method_field('post') }}
+            {{ csrf_field() }}
+            <div class="form-group">
+                <label for="inputName">
+                    Name
+                </label>
+                <div class="input-group Name">
+                    <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span><input type="text" class="form-control" id="inputName" name="inputName" value="{{ $user->name }}">
+                </div>
+                <label for="inputEmail">
+                    Email
+                </label>
+                <div class="input-group Email">
+                    <span class="input-group-addon"><i class="glyphicon glyphicon-envelope"></i></span><input type="text" class="form-control" id="inputEmail" name="inputEmail" value="{{ $user->email }}">
+                </div>
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>		
+    </div>
 </div>
 
 @endsection
