1e8b5daf9c64ff7759a12623218712a3e0062795
Juha Remes <jremes@outlook.com>
2020-09-28 23:29:13 +0100

    Admin categories.blade.php: tabs to spaces

--- resources/views/admin/categories.blade.php
+++ resources/views/admin/categories.blade.php
@@ -2,37 +2,37 @@
 
 @section('content')
 <div class="container">
-	<div class="row" style="max-width: 90%">
-		@if(session()->has('message'))
-		<div class="alert alert-success">
-			{{ session()->get('message') }}
-		</div>
-		@endif
-		<a class="btn btn-primary btn-sm pull-left" href="{{ route('admin.categories.create') }}" role="button">New Category</a> 
-		<table class="table table-hover">
-		  <thead>
-		    <tr>
-			  	<th scope="col">#</th>
-			  	<th scope="col">Category</th>
-			  	<th scope="col">Description</th>
-			  	<th scope="col">Actions</th>
-		    </tr>
-		  </thead>
-		  <tbody>
-		  	@foreach($categories as $category)
-		  	<tr>
-		  		<th scope="row">{{ $category->id }}</th>
-		  		<td>{{ $category->category }}</td>
-		  		<td>{{ $category->description }}</td>
-		  		<td>
-		  			<a href="{{ route('admin.categories.edit', $category->id) }}" class="btn btn-info">Edit</a>
-		  			<a href="{{ route('admin.categories.delete', $category->id) }}" class="btn btn-danger">Delete</a>
-		  		</td>
-		  	</tr>
-		  	@endforeach
-		  </tbody>
-		</table>
-	</div>
+    <div class="row" style="max-width: 90%">
+        @if(session()->has('message'))
+        <div class="alert alert-success">
+            {{ session()->get('message') }}
+        </div>
+        @endif
+        <a class="btn btn-primary btn-sm pull-left" href="{{ route('admin.categories.create') }}" role="button">New Category</a> 
+        <table class="table table-hover">
+          <thead>
+            <tr>
+                  <th scope="col">#</th>
+                  <th scope="col">Category</th>
+                  <th scope="col">Description</th>
+                  <th scope="col">Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+              @foreach($categories as $category)
+              <tr>
+                  <th scope="row">{{ $category->id }}</th>
+                  <td>{{ $category->category }}</td>
+                  <td>{{ $category->description }}</td>
+                  <td>
+                      <a href="{{ route('admin.categories.edit', $category->id) }}" class="btn btn-info">Edit</a>
+                      <a href="{{ route('admin.categories.delete', $category->id) }}" class="btn btn-danger">Delete</a>
+                  </td>
+              </tr>
+              @endforeach
+          </tbody>
+        </table>
+    </div>
 </div>
 
 @endsection
