224a838855b6a031667f0b5c3faf7455c5b4de29
Juha Remes <jremes@outlook.com>
2020-09-30 23:55:27 +0100

    Admin users.blade.php: tabs to spaces

--- resources/views/admin/users.blade.php
+++ resources/views/admin/users.blade.php
@@ -2,36 +2,36 @@
 
 @section('content')
 <div class="container">
-	<div class="row" style="max-width: 90%">
-		@if(session()->has('message'))
-			<div class="alert alert-success">
-				{{ session()->get('message') }}
-			</div>
-		@endif
-		<table class="table table-hover">
-			<thead>
-				<tr>
-					<th scope="col">#</th>
-					<th scope="col">Name</th>
-					<th scope="col">E-mail</th>
-					<th scope="col">Actions</th>
-				</tr>
-			</thead>
-			<tbody>
-				@foreach($users as $user)
-					<tr>
-						<th scope="row">{{ $user->id }}</th>
-						<td scope="row">{{ $user->name }}</td>
-						<td scope="row">{{ $user->email }}</td>
-						<td scope="row">
-							<a href="{{ route('admin.users.edit', $user->id) }}" class="btn btn-info">Edit</a>
-							<a href="{{ route('admin.users.delete', $user->id) }}" class="btn btn-danger">Delete</a>
-						</td>
-					</tr>
-				@endforeach
-			</tbody>
-		</table>
-	</div>
+    <div class="row" style="max-width: 90%">
+        @if(session()->has('message'))
+            <div class="alert alert-success">
+                {{ session()->get('message') }}
+            </div>
+        @endif
+        <table class="table table-hover">
+            <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Name</th>
+                    <th scope="col">E-mail</th>
+                    <th scope="col">Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($users as $user)
+                    <tr>
+                        <th scope="row">{{ $user->id }}</th>
+                        <td scope="row">{{ $user->name }}</td>
+                        <td scope="row">{{ $user->email }}</td>
+                        <td scope="row">
+                            <a href="{{ route('admin.users.edit', $user->id) }}" class="btn btn-info">Edit</a>
+                            <a href="{{ route('admin.users.delete', $user->id) }}" class="btn btn-danger">Delete</a>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
 </div>
 
 @endsection
