0c484d3b5adc77df85ffa99b068ea5179d4e3cf9
Juha Remes <jremes@outlook.com>
2020-10-14 22:02:39 +0100

    Pluck user_id

--- app/Http/Controllers/SettingsController.php
+++ app/Http/Controllers/SettingsController.php
@@ -3,12 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\User;
 use Auth;
+
 
 class SettingsController extends Controller
 {
     public function index()
     {
+        $user_id = Auth::user()->id;
+        $api_token = User::where('id', $user_id)->pluck('api_token');
         return view('settings');
     }
 }
