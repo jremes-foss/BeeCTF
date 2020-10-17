221bc8e7caccf2855cc2644644d20d8f991e7136
Juha Remes <jremes@outlook.com>
2020-10-17 20:46:46 +0100

    Add basic form structure for API Token in settings.blade.php

--- resources/views/settings.blade.php
+++ resources/views/settings.blade.php
@@ -3,7 +3,17 @@
 
 <div class="container">
     <div class="row" style="max-width: 90%">
-    <!-- TODO -->
+        <div class="col-md-8">
+            <form class="form form-horizontal" role="form" method="post" enctype="multipart/form-data" action="#">
+                <div class="form-group">
+                    <label for="inputApiToken">API Token</label>
+                    <div class="input-group ApiToken">
+                        <span class="input-group-addon"><i class="glyphicon glyphicon-console"></i></span>
+                        <input type="text" class="form-control" id="inputScore" name="inputScore" value="{{ $api_token }}">
+                    </div>
+                </form>
+            </form>
+        </div>
     </div>
 </div>
 
