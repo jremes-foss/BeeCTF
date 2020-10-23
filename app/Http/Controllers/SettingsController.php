15ffa3b5d8ce98d5df4e58c9e517ec45388164ba
Juha Remes <jremes@outlook.com>
2020-10-23 22:33:31 +0100

    Switch the get request into Str::random

--- app/Http/Controllers/SettingsController.php
+++ app/Http/Controllers/SettingsController.php
@@ -21,7 +21,7 @@ class SettingsController extends Controller
     {
         $user_id = Auth::user()->id;
         $user = User::where('id', $user_id)->first();
-        $user->api_token = $request->get('inputApiToken');
+        $user->api_token = Str::random(60);
         $user->save();
         return redirect()->route('settings')->with('success', 'API Token updated!');
     }
