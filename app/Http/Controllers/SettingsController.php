35d9def996b3472895565c5fd7a950d23c1d198a
Juha Remes <jremes@outlook.com>
2020-10-24 20:58:06 +0100

    Remove the redirect

--- app/Http/Controllers/SettingsController.php
+++ app/Http/Controllers/SettingsController.php
@@ -23,6 +23,5 @@ class SettingsController extends Controller
         $user = User::where('id', $user_id)->first();
         $user->api_token = Str::random(60);
         $user->save();
-        // return redirect()->route('settings')->with('success', 'API Token updated!');
     }
 }
