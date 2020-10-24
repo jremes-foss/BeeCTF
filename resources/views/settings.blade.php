14f7a5837dcdcf9f82c4f7d82c7df7aa2dc02a53
Juha Remes <jremes@outlook.com>
2020-10-24 19:51:01 +0100

    Refactor the settings form into AJAX

--- resources/views/settings.blade.php
+++ resources/views/settings.blade.php
@@ -5,18 +5,39 @@
     <div class="row" style="max-width: 90%">
         <div class="col-md-8">
             <form class="form form-horizontal" role="form" method="post" enctype="multipart/form-data" action="{{ route('user.updateApiToken') }}">
-            {{ method_field('post') }}
-            {{ csrf_field() }}
                 <div class="form-group">
                     <label for="inputApiToken">API Token</label>
                     <div class="input-group ApiToken">
                         <span class="input-group-addon"><i class="glyphicon glyphicon-console"></i></span>
                         <input type="text" class="form-control" id="inputApiToken" name="inputApiToken" value="{{ $api_token }}">
                     </div>
-                <button type="submit" class="btn btn-primary">Submit</button>
+                <button id="regenerate" class="btn btn-primary">Regenerate</button>
             </form>
         </div>
     </div>
 </div>
 
+<script>
+    $(function() {
+        $.ajaxSetup({
+            headers: { 
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            }
+        });
+
+        $('#regenerate').click(function(e) {
+            $.ajax({
+                url: "{{ route('user.updateApiToken') }}",
+                type: "POST",
+                dataType: "json",
+                success: function(data) {
+                    console.log(data);
+                },
+                error: function(data) {
+                    console.log("ERROR:" + data);
+                }
+            });
+        });
+    });
+</script>
 @endsection
