5ba4f7086f6665318d16fc3900e8cff9b9eeae5d
Juha Remes <jremes@outlook.com>
2020-10-31 13:41:26 +0000

    Introduce methods to pull scoreboard through API

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Category;
 use App\Challenge;
+use App\Solved;
+use App\User;
 
 class ApiController extends Controller
 {
@@ -45,7 +47,53 @@ class ApiController extends Controller
      */
     public function getScoreBoard()
     {
+        $users = User::where('user_type', 'User')->get([
+            'id', 'name'
+        ]);
+
+        $score_array = [];
+
+        foreach ($users as $user) {
+            $score = $this->getScoresPerPlayer($user->id);
+            $id = $user->id;
+            $name = $user->name;
+            $temp_array = array(
+                'id' => $id,
+                'name' => $name,
+                'score' => $score
+            );
+            array_push($score_array, $temp_array);
+        }
+        $score_collection = collect($score_array);
+        $score_sorted = $score_collection->sortByDesc('score');
+
+        return $score_sorted;
     }
+
+    /**
+     * Fetches the scores per player in JSON format.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    private function getScoresPerPlayer($id)
+    {
+        $score = 0;
+
+        $solutions = Solved::where('user_id', $id)->get([
+            'challenge_id',
+            'user_id'
+        ]);
+
+        foreach ($solutions as $solution) {
+            $points = Challenge::where('id', $solution['challenge_id'])->get();
+            foreach ($points as $point) {
+                $score += $point->score;
+            }
+        }
+
+        return $score;
+    }
+
 
     /**
      * Pings the application as a health check.
