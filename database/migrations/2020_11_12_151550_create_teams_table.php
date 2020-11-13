6ed78c63306bd5387c0eda9746ddeb2b80e7c8a6
Juha Remes <jremes@outlook.com>
2020-11-13 10:12:25 +0000

    Add name string to migration

--- database/migrations/2020_11_12_151550_create_teams_table.php
+++ database/migrations/2020_11_12_151550_create_teams_table.php
@@ -15,7 +15,7 @@ class CreateTeamsTable extends Migration
     {
         Schema::create('teams', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->timestamps();
+            $table->string('name');
         });
     }
 
