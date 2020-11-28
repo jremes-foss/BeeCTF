9366da16b00f3a6702f6d6196401cd32b287c5f8
Juha Remes <jremes@outlook.com>
2020-11-28 19:42:38 +0000

    Add table heading for teams

--- resources/views/teams.blade.php
+++ resources/views/teams.blade.php
@@ -2,6 +2,8 @@
 @section('content')
 <div class="container">
     <div class="row" style="max-width: 90%">
+        <table class="table table-hover">
+        </table>
     </div>
 </div>
 @endsection
