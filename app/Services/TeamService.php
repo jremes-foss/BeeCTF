ee9c08810469d6f19c21e46dbea70f2789c72325
Juha Remes <jremes@outlook.com>
2020-12-30 12:58:16 +0000

    Join to player_team table

--- app/Services/TeamService.php
+++ app/Services/TeamService.php
@@ -10,7 +10,9 @@ class TeamService
 {
     public function getTeamPlayers($team)
     {
-        $users = User::where('user_type', 'User')->get();
+        $users = User::where('user_type', 'User')
+            ->join('player_team', 'id', '=', 'player_id')
+            ->get();
         return $users;
     }
 }
