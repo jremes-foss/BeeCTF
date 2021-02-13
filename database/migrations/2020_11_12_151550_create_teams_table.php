c87025c93e1745d86ada2eb7ec5127210f4a9ffa
Juha Remes <jremes@outlook.com>
2021-02-13 23:17:14 +0000

    Add timestamps column

--- database/migrations/2020_11_12_151550_create_teams_table.php
+++ database/migrations/2020_11_12_151550_create_teams_table.php
@@ -16,6 +16,7 @@ class CreateTeamsTable extends Migration
         Schema::create('teams', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name');
+            $table->timestamps();
         });
     }
 
