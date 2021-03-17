2ddc3f06f7cd5f85fc762b445924c8969f1a6a1d
Juha Remes <jremes@outlook.com>
2021-03-17 19:53:22 +0000

    Create admin user for testStore

--- tests/Unit/TeamTest.php
+++ tests/Unit/TeamTest.php
@@ -35,6 +35,8 @@ class TeamTest extends TestCase
 
     public function testStore()
     {
-        
+        $admin = factory(\App\User::class)
+            ->states('admin')
+            ->create();
     }
 }
