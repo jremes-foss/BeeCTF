1eeec80c7c5a2e63afe93a3adc7979c2b99fedbf
Juha Remes <jremes@outlook.com>
2021-03-28 18:38:01 +0100

    Replace Model::class with Team:class

--- database/factories/TeamFactory.php
+++ database/factories/TeamFactory.php
@@ -2,10 +2,10 @@
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
-use App\Model;
+use App\Team;
 use Faker\Generator as Faker;
 
-$factory->define(Model::class, function (Faker $faker) {
+$factory->define(Team::class, function (Faker $faker) {
     return [
         'name' => 'T3stT34m'
     ];
