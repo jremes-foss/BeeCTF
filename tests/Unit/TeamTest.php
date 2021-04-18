840bd845340e620e9d3b79a84b36c7964ce159c6
Juha Remes <jremes@outlook.com>
2021-04-18 20:07:09 +0100

    Assign to variable

--- tests/Unit/TeamTest.php
+++ tests/Unit/TeamTest.php
@@ -28,7 +28,7 @@ class TeamTest extends TestCase
         // dd(TeamPlayer::all());
         $team_id = 1;
         
-        app('App\Services\TeamService')->getTeamPlayers($team_id);
+        $team_players = app('App\Services\TeamService')->getTeamPlayers($team_id);
     }
 
     public function testIndex()
