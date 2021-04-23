8f6a98e79785d571be815b8a87811d6ee43ba9ea
Juha Remes <jremes@outlook.com>
2021-04-23 19:30:38 +0100

    Specify table name

--- app/Services/TeamService.php
+++ app/Services/TeamService.php
@@ -8,7 +8,7 @@ class TeamService
 {
     public function getTeamPlayers($team_id)
     {
-        $users = User::where('user_type', 'User')
+        $users = User::where('users.user_type', 'User')
             ->join('player_team', 'users.id', '=', 'player_team.player_id')
             ->where('player_team.team_id', $team_id)
             ->get();
