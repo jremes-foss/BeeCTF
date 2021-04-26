5b2457b0fec6d96d164927f415b0e0d00df8b8e3
Juha Remes <jremes@outlook.com>
2021-04-27 00:23:04 +0100

    Fix the failing unit test

--- app/Services/TeamService.php
+++ app/Services/TeamService.php
@@ -3,12 +3,13 @@
 namespace App\Services;
 
 use App\User;
+use Illuminate\Support\Facades\DB;
 
 class TeamService 
 {
     public function getTeamPlayers($team_id)
     {
-        $users = User::where('users.user_type', '!=', 'Administrator')
+        $users = DB::table('users')
             ->join('player_team', 'users.id', '=', 'player_team.player_id')
             ->where('player_team.team_id', $team_id)
             ->get();
