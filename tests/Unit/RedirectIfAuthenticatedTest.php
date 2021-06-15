653123a6e3aecfa1c677319f813dce7c0ea5a57b
Juha Remes <jremes@outlook.com>
2021-06-15 10:50:35 +0100

    Write the new tests properly, this should do it

--- tests/Unit/RedirectIfAuthenticatedTest.php
+++ tests/Unit/RedirectIfAuthenticatedTest.php
@@ -3,9 +3,13 @@
 namespace Tests\Unit;
 
 use Tests\TestCase;
+use App\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class RedirectIfAuthenticatedTest extends TestCase 
 {
+    use RefreshDatabase;
+
     public function testNotAuthenticated()
     {
         $response = $this->get('/login');
