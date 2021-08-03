b478749fe931c78b62d2706b623d8a83f942c34d
Juha Remes <jremes@outlook.com>
2021-08-03 22:27:12 +0100

    Import RefreshDatabase class

--- tests/Unit/TeamPlayerTest.php
+++ tests/Unit/TeamPlayerTest.php
@@ -2,9 +2,12 @@
 
 namespace Tests\Unit;
 
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class TeamPlayerTest extends TestCase
 {
+    use RefreshDatabase;
+    
     // TODO
 }
