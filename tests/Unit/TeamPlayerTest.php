a5e80502cd0455057b03b0b4ede39e8ba06e1d7c
Juha Remes <jremes@outlook.com>
2021-08-31 19:41:31 +0100

    Remove blank line before brace in TeamPlayerTest.php

--- tests/Unit/TeamPlayerTest.php
+++ tests/Unit/TeamPlayerTest.php
@@ -65,6 +65,5 @@ class TeamPlayerTest extends TestCase
         $team_test = Team::find($team->id);
         $this->assertNotNull($team_test);
         $this->assertEquals($team_test->id, $team_player->teams->first()->id);
-
     }
 }
