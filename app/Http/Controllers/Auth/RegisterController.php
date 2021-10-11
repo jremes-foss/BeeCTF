584b6d6b921d8508d165542fd51e51a8d54e2f0b
Juha Remes <jremes@outlook.com>
2021-10-11 20:56:08 +0100

    Assign user model to variable

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -64,7 +64,7 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        return User::create([
+        $user = User::create([
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => bcrypt($data['password']),
