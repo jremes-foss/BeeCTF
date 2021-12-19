70d80490a506b10cd340eda688ef13505dbd36df
Juha Remes <jremes@outlook.com>
2021-12-19 00:39:36 +0000

    Comment out the code for refactor

--- app/Services/TeamService.php
+++ app/Services/TeamService.php
@@ -27,26 +27,25 @@ class TeamService
         $team_players = [];
         $solved = [];
 
-        foreach ($teams as $team) {
-            $teamid = $team->id;
-            array_push($team_ids, $teamid);
-        }
+        // foreach ($teams as $team) {
+        //     $teamid = $team->id;
+        //     array_push($team_ids, $teamid);
+        // }
 
-        foreach ($team_ids as $team_id) {
-            $tp_get = TeamPlayer::where('team_id', $team_id)->get();
-            array_push($team_players, $tp_get);
-        }
+        // foreach ($team_ids as $team_id) {
+        //     $tp_get = TeamPlayer::where('team_id', $team_id)->get();
+        //     array_push($team_players, $tp_get);
+        // }
 
-        foreach ($team_players as $team_player) {
-            $player_id = $team_player->player_id;
-            $solved_challenges = Score::where('user_id', $player_id)
-                ->get()
-                ->unique('challenge_id');
-            array_push($solved, $solved_challenges);
-        }
+        // foreach ($team_players as $team_player) {
+        //     $player_id = $team_player->player_id;
+        //     $solved_challenges = Score::where('user_id', $player_id)
+        //         ->get()
+        //         ->unique('challenge_id');
+        //     array_push($solved, $solved_challenges);
+        // }
 
-        // Pick unique values only!
-        foreach ($solved_challenges as $challenge) {
-        }
+        // foreach ($solved_challenges as $challenge) {
+        // }
     }
 }
