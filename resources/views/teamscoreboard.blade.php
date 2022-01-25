9f9226bc9b406a8f60336ec5ef1d0a35e3197ece
Juha Remes <jremes@outlook.com>
2022-01-25 22:03:39 +0000

    Add table structure

--- resources/views/teamscoreboard.blade.php
+++ resources/views/teamscoreboard.blade.php
@@ -2,6 +2,26 @@
 @section('content')
 
 <div class="container">
+    <div class="row" style="max-width: 90%">
+        <table class="table table-hover">
+            <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Team</th>
+                <th scope="col">Score</th>
+            </tr>
+            </thead>
+            <tbody>
+            @foreach($scores as $score)
+            <tr>
+                <td>{{ $loop->iteration }}</td>
+                <td>{{ $score["name"] }}</td>
+                <td>{{ $score["score"] }}</td>
+            </tr>
+            @endforeach
+            </tbody>
+        </table>
+    </div>
 </div>
 
 @endsection
