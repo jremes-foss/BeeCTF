cc498f068992565d7fdecc7fdee50a84bf3e77dc
Juha Remes <jremes@outlook.com>
2022-12-15 23:50:20 +0000

    index: replace Redirect with RedirectResponse

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -10,7 +10,7 @@ class CategoriesController extends Controller
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\RedirectResponse
      */
     public function index()
     {
