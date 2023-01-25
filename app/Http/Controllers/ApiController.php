e11ea60ff57d6bc03cdd4af61dd3e316fb2a4100
Juha Remes <jremes@outlook.com>
2023-01-25 23:53:17 +0000

    Return array

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -57,7 +57,7 @@ class ApiController extends Controller
     /**
      * Fetches the scoreboard in JSON format.
      *
-     * @return TODO
+     * @return array
      */
     public function getScoreBoard()
     {
