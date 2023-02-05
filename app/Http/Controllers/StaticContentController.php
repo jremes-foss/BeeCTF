dd27102c06bd0e2eef3751444da40003311a7829
Juha Remes <jremes@outlook.com>
2023-02-05 19:30:01 +0000

    Add PHPDoc to StaticContentController

--- app/Http/Controllers/StaticContentController.php
+++ app/Http/Controllers/StaticContentController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 class StaticContentController extends Controller
 {
+    /**
+     * TODO
+     */
     public function index()
     {
         return view('scontent');
